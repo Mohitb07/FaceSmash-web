39e326b8a2ca7a237e520f91fbcacd91397bccd1
Mohitb07 <bmohit980@gmail.com>
2022-12-02 22:40:57 +0530

    initial setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-primary-100 text-5xl">Hello world</h1>
+    </div>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontSize: {
+      xs: '0.75rem',
+      sm: '0.875rem',
+      base: '1rem',
+      lg: '1.125rem',
+      xl: '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '4rem',
+    },
+    extend: {
+      colors: {
+        primary: {
+          100: '#6e3ddc',
+          200: '#6337c6',
+          300: '#5831b0',
+          400: '#4d2b9a',
+          500: '#422584',
+          600: '#371f6e',
+          700: '#2c1858',
+          800: '#211242',
+          900: '#160c2c',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
