444d7dd9c5ec8bcae878ff2e505187b8d69202bb
Mohitb07 <bmohit980@gmail.com>
2022-12-03 22:07:00 +0530

    build: app config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ['static.cdninstagram.com'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ src/utils/AppConfig.ts
@@ -0,0 +1,10 @@
+// FIXME: Update this configuration file based on your project information
+
+export const AppConfig = {
+    site_name: 'FaceSmash',
+    title: 'FaceSmash',
+    description:
+      "It's a social media platform for users to interact with their friends",
+    locale: 'en',
+  };
+  
