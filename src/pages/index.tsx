cf5d5d56c58c50edc975f96330ed93f772d53a6b
Mohitb07 <bmohit980@gmail.com>
2022-12-03 22:09:37 +0530

    refactor: page container and meta component used

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,19 @@
+import { Meta } from '../layouts/Meta';
+import {Main} from '../templates/Main';
+
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-primary-100 text-5xl">Hello world</h1>
-    </div>
-  )
+    <Main
+    meta={
+      <Meta
+        title="FaceSmash"
+        description="It's a social media platform for users to interact with their friends."
+      />
+    }
+    >
+      <div>
+        <h1 className="text-primary-100 text-5xl">Hello world</h1>
+      </div>
+    </Main>
+  );
 }
