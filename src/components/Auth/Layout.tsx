9166780cf54d357240ac0cb0786d0f09e3ef77c4
Mohitb07 <bmohit980@gmail.com>
2022-12-05 21:31:19 +0530

    feat: layout component for auth pages

---
+++ src/components/Auth/Layout.tsx
@@ -0,0 +1,78 @@
+import { Main } from '../../templates/Main';
+import Image from 'next/image';
+import React from 'react';
+import { Meta } from '../../layouts/Meta';
+import Link from 'next/link';
+
+type LayoutProps = {
+  children: React.ReactNode;
+  meta: string;
+  footerText: string;
+  footerLabel: string;
+  footerLink: string;
+  containerStyle?: string;
+};
+
+const Layout: React.FC<LayoutProps> = ({
+  children,
+  meta = '',
+  footerText = '',
+  footerLabel = '',
+  footerLink = '',
+  containerStyle = '',
+}) => {
+  return (
+    <Main
+      meta={
+        <Meta
+          title={`FaceSmash - ${meta}`}
+          description="It's a social media platform for users to interact with their friends."
+        />
+      }
+    >
+      <div className="min-h-screen flex items-center justify-center">
+        <div className="hidden lg:flex  items-center justify-center pt-16">
+          <Image
+            src="https://static.cdninstagram.com/rsrc.php/v3/y4/r/ItTndlZM2n2.png"
+            alt=""
+            height={800}
+            width={600}
+          />
+        </div>
+        <div>
+          <div className={`w-screen h-screen md:h-[600px] md:w-[500px] bg-[#19181A] p-5 md:p-16 ${containerStyle}`}>
+            <header>
+              <h1 className="text-center text-5xl font-bold text-white">
+                <span className="text-primary-100">Face</span>Smash
+              </h1>
+            </header>
+            <div className='flex-1'>
+              {children}
+            </div>
+            <div className="md:hidden text-center mt-10">
+              <Link href={footerLink}>
+                <h1 className="text-sm text-[#8E8FAB]">
+                  {footerText}{' '}
+                  <span className="text-primary-100 font-bold">
+                    {footerLabel}
+                  </span>
+                </h1>
+              </Link>
+            </div>
+          </div>
+          <div className="h-20 mt-5 w-[500px] bg-[#19181A] items-center justify-center hidden md:flex">
+            <Link href={footerLink}>
+              <h1 className="text-lg text-[#8E8FAB]">
+                {footerText}{' '}
+                <span className="text-primary-100 font-bold">
+                  {footerLabel}
+                </span>
+              </h1>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </Main>
+  );
+};
+export default Layout;
