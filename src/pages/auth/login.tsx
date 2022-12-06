f0ba62f1180536a0530e54a6b64a23abc4d0d2e6
Mohitb07 <bmohit980@gmail.com>
2022-12-06 21:12:29 +0530

    refactor: code refactored

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -11,17 +11,17 @@ const Login = () => {
       footerLabel="Sign Up"
       footerLink="/auth/register"
     >
-      <div className="mt-5 flex flex-col space-y-5">
+      <div className="mt-5 flex flex-col space-y-2">
         <div className='flex flex-col space-y-1'>
           <label htmlFor="" className='text-[#8E8FAB]'>Email</label>
           <Input type="email" placeholder="Email" />
         </div>
 
-        <div className='flex flex-col space-y-1'>
+        <div className='flex flex-col space-y-1 pb-10'>
           <label htmlFor="" className='text-[#8E8FAB]'>Password</label>
           <Input type="password" placeholder="Password" />
         </div>
-        <Button label="Log in" />
+        <Button onClick={() => {}} label="Log in" />
       </div>
     </AuthLayout>
   );
