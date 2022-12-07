6a7a7afff8c4c0d7346f71af26a12ca253c2bbd0
Mohitb07 <bmohit980@gmail.com>
2022-12-07 21:28:51 +0530

    feat: side bar added to homepage init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,27 @@
+import Sidebar from '../components/Sidebar';
 import { Meta } from '../layouts/Meta';
-import {Main} from '../templates/Main';
+import { Main } from '../templates/Main';
 
 export default function Home() {
   return (
     <Main
-    meta={
-      <Meta
-        title="FaceSmash"
-        description="It's a social media platform for users to interact with their friends."
-      />
-    }
+      meta={
+        <Meta
+          title="FaceSmash"
+          description="It's a social media platform for users to interact with their friends."
+        />
+      }
     >
-      <div>
-        <h1 className="text-primary-100 text-5xl">Hello world</h1>
+      <div className="flex">
+        <Sidebar />
+        <div>
+          <main>
+            <h1 className="text-primary-100">Center</h1>
+          </main>
+          <aside>
+            <h1>Right Hand side</h1>
+          </aside>
+        </div>
       </div>
     </Main>
   );
