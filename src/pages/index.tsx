5fa0a5bed3cb78dfffb22119b4188eba3d42f331
Mohitb07 <bmohit980@gmail.com>
2022-12-11 21:39:39 +0530

    feat: search input added and code refactored

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
+import { BsSearch } from 'react-icons/bs';
 import Feed from '../components/Feed';
-import Sidebar from '../components/Sidebar';
 import User from '../components/User';
 import { Meta } from '../layouts/Meta';
 import { Main } from '../templates/Main';
@@ -14,31 +14,46 @@ export default function Home() {
         />
       }
     >
-      <div className="flex h-screen">
-        {/* <div className="">
-          <Sidebar />
-        </div> */}
-        <div className="flex-1">
+      <div className="h-screen">
+        <div className="">
+          <div className="block md:hidden text-center">
+            <h1 className="text-[2rem] font-bold text-white">
+              <span className="text-primary-100">Face</span>Smash
+            </h1>
+          </div>
+          <div className="px-5">
+            <div className="flex w-full md:hidden py-4 px-2 my-5 items-center bg-gray-800 rounded-md">
+              <BsSearch className="text-xl text-slate-300 mx-3" />
+              <input
+                className="w-full bg-transparent outline-none border-none"
+                type="text"
+                placeholder="Search User..."
+              />
+            </div>
+          </div>
           <div className="flex md:p-10 justify-center items-start gap-10">
-            <main className="space-y-5 md:ml-[20%] xl:ml-[10%]">
+            <main className="space-y-5 pb-16 md:ml-[20%] xl:ml-[10%]">
               <Feed />
               <Feed />
               <Feed />
               <Feed />
+              <>
+                <p className='text-center text-gray-500'>End Of Result</p>
+              </>
             </main>
             <aside className="hidden lg:flex flex-col">
               <div className="flex  items-center gap-5 mt-10">
                 <User size="large" />
               </div>
-              <div className='mt-5'>
+              <div className="mt-5">
                 <p className="text-xl text-gray-500 font-semibold">
                   Suggestions for you
                 </p>
                 <div>
-                  <User/>
-                  <User/>
-                  <User/>
-                  <User/>
+                  <User />
+                  <User />
+                  <User />
+                  <User />
                 </div>
               </div>
             </aside>
