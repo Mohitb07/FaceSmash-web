9faa2ce6d609416513fb8078f17478fa44c2bffc
Mohitb07 <bmohit980@gmail.com>
2022-12-11 21:39:00 +0530

    feat: bottom navigation displayed on smaller screens

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,18 @@
 import '../../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Sidebar from '../components/Sidebar';
+import BottomNavigation from '../components/BottomNavigation';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <div className="">
+    <div className='relative'>
+      <div>
         <Sidebar />
       </div>
+      <div className='bg-slate-900 fixed z-50 bottom-0 w-full'>
+        <BottomNavigation/>
+      </div>
       <Component {...pageProps} />
-    </>
+    </div>
   );
 }
