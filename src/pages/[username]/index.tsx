cbf0f039d74e5562ed513312ed4bbf1bbea15bef
Mohitb07 <bmohit980@gmail.com>
2022-12-11 21:37:34 +0530

    refactor: code refactored and connections added in smaller screens

--- src/pages/[username]/index.tsx
+++ src/pages/[username]/index.tsx
@@ -18,8 +18,8 @@ const UserProfile = () => {
         />
       }
     >
-      <div className="flex flex-col justify-start md:justify-center space-y-10 items-center md:p-10 lg:ml-[10%] xl:ml-0">
-        <div className='flex items-center gap-5 lg:gap-10 xl:gap-20 p-3'>
+      <div className="flex flex-col justify-start md:justify-center space-y-5 md:space-y-10 items-center md:p-10 lg:ml-[10%] xl:ml-0">
+        <div className="flex items-center gap-5 lg:gap-10 xl:gap-20 p-3">
           <div className="h-[100px] w-[100px] md:h-[180px] md:w-[180px] xl:h-[200px] xl:w-[200px]">
             <Avatar
               url={
@@ -37,24 +37,24 @@ const UserProfile = () => {
               <Button
                 label="Edit profile"
                 onClick={() => {}}
-                style="hidden md:block bg-white text-black text-base lg:text-md xl:text-[1.2rem] px-4"
+                style="hidden md:block text-base lg:text-md xl:text-[1.1rem] px-4 bg-[#fff] text-black"
               />
               <FiSettings className="text-xl xl:text-3xl" />
             </div>
             <Button
               label="Edit profile"
               onClick={() => {}}
-              style="block md:hidden bg-white text-black text-base lg:text-md xl:text-[1.2rem] px-4"
+              style="block md:hidden text-base lg:text-md xl:text-[1.2rem] px-4 bg-[#fff] text-black"
             />
             <div className="text-lg hidden items-center gap-5 md:flex">
               <p>
-                <span className="font-semibold">0</span> followings
+                <span className="font-semibold mr-2">0</span> posts
               </p>
               <p>
-                <span className="font-semibold">0</span> posts
+                <span className="font-semibold mr-2">0</span> followers
               </p>
               <p>
-                <span className="font-semibold">0</span> followers
+                <span className="font-semibold mr-2">0</span> followings
               </p>
             </div>
             <div className="hidden md:flex">
@@ -62,11 +62,28 @@ const UserProfile = () => {
             </div>
           </div>
         </div>
-        <div className='space-y-5'>
+        <div className="h-[5rem] grid grid-cols-3 border-y border-slate-700 place-items-center w-full p-1 md:hidden">
+          <div className="text-center">
+            <span className="font-semibold">1230</span>
+            <p className="text-slate-400">posts</p>
+          </div>
+          <div className="text-center">
+            <span className="font-semibold">120k</span>
+            <p className="text-slate-400">followers</p>
+          </div>
+          <div className="text-center">
+            <span className="font-semibold">100</span>
+            <p className="text-slate-400">following</p>
+          </div>
+        </div>
+        <div className="space-y-5 pb-16">
           <Feed />
           <Feed />
           <Feed />
           <Feed />
+          <>
+            <p className="text-center text-gray-500">End Of Result</p>
+          </>
         </div>
       </div>
     </Main>
