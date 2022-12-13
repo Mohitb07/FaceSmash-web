802f6006f53eba2e1811167ab63dfbcee0ba4575
Mohitb07 <bmohit980@gmail.com>
2022-12-13 19:51:34 +0530

    refactor: styles props added to avatar

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -5,12 +5,13 @@ type AvatarProps = {
   url: string;
   height?: number;
   width?: number;
+  styles?: string
 };
 
-const Avatar = ({ url, height=30, width=30 }: AvatarProps) => {
+const Avatar = ({ url, height=30, width=30, styles = "" }: AvatarProps) => {
   return (
     <Image
-      className="rounded-full"
+      className={`rounded-full ${styles}`}
       src={url}
       height={height}
       width={width}
