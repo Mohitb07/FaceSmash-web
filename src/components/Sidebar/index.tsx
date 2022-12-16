2048ae062aa1722f2aee8f3baa38025ad8ddd6ad
Mohitb07 <bmohit980@gmail.com>
2022-12-16 21:26:11 +0530

    refactor: nav item refactored and modal body added init

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { TiHome } from 'react-icons/ti';
 import { BsSearch } from 'react-icons/bs';
 import { RxHamburgerMenu } from 'react-icons/rx';
+import { HiOutlinePlusCircle } from 'react-icons/hi';
 import Link from 'next/link';
 import {
   Menu,
   MenuButton,
   MenuList,
   MenuItem,
-  useDisclosure,
+  Button,
+  Input,
+  Box,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  Textarea,
 } from '@chakra-ui/react';
 import {
   Drawer,
@@ -18,13 +25,46 @@ import {
   DrawerOverlay,
   DrawerContent,
 } from '@chakra-ui/react';
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from '@chakra-ui/react';
 
 import Avatar from '../Avatar';
 import Brand from '../Brand';
 import User from '../User';
+import NavItem from '../NavItem';
 
 const Sidebar = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [isSearchDrawerOpen, setIsSearchDrawerOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [postValue, setPostValue] = useState({
+    title: '',
+    description: '',
+  });
+
+  const searchDrawerClose = () => {
+    if (isSearchDrawerOpen) {
+      setIsSearchDrawerOpen(false);
+    }
+  };
+  const modalClose = () => {
+    if (isModalOpen) {
+      setIsModalOpen(false);
+    }
+  };
+
+  const handlePostValueChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setPostValue(prev => ({
+      ...prev,
+      [e.target.name]: e.target.value
+    }))
+  };
 
   return (
     <div className="hidden md:flex fixed h-full flex-col justify-between bg-[#0b0b0b] pt-[5rem] px-[2rem] pb-[2rem]">
@@ -42,25 +82,30 @@ const Sidebar = () => {
         <ul className="space-y-12 text-xl">
           {/* VscHome */}
           <Link href="/">
-            <li className="nav-item group">
-              <TiHome className="text-3xl hover-animation" />{' '}
-              <span className="hidden lg:block">Home</span>
-            </li>
+            <NavItem
+              icon={<TiHome className="text-3xl hover-animation" />}
+              label="Home"
+            />
           </Link>
-          <li className="nav-item group" onClick={onOpen}>
-            <BsSearch className="text-2xl hover-animation" />{' '}
-            <span className="hidden lg:block">Search</span>
-          </li>
+          <NavItem
+            onClick={() => setIsSearchDrawerOpen(true)}
+            icon={<BsSearch className="text-2xl hover-animation" />}
+            label="Search"
+          />
+          <NavItem
+            onClick={() => setIsModalOpen(true)}
+            icon={<HiOutlinePlusCircle className="text-2xl hover-animation" />}
+            label="Create"
+          />
           <Link href="/mohitbisht1903">
-            <li className="nav-item group">
+            <NavItem label="Profile">
               <Avatar
                 styles="hover-animation"
                 url={
                   'https://lh3.googleusercontent.com/ogw/AOh-ky2wAgtbl4h_XUEs5x-5xfgBLXa_Aq0k6ahwaOxCgw=s32-c-mo'
                 }
-              />{' '}
-              <span className="hidden lg:block">Profile</span>
-            </li>
+              />
+            </NavItem>
           </Link>
         </ul>
       </nav>
@@ -81,7 +126,12 @@ const Sidebar = () => {
           </MenuList>
         </Menu>
       </div>
-      <Drawer placement="left" onClose={onClose} isOpen={isOpen} size="md">
+      <Drawer
+        placement="left"
+        onClose={searchDrawerClose}
+        isOpen={isSearchDrawerOpen}
+        size="md"
+      >
         <DrawerOverlay />
         <DrawerContent>
           <DrawerHeader borderBottomWidth="1px">
@@ -105,6 +155,61 @@ const Sidebar = () => {
           </DrawerBody>
         </DrawerContent>
       </Drawer>
+      <Modal isOpen={isModalOpen} onClose={modalClose} size="4xl">
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Create new post</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Box p={1}>
+              <FormControl isRequired isInvalid={false}>
+                <FormLabel>Title</FormLabel>
+                <Input
+                  autoFocus
+                  name='title'
+                  focusBorderColor="brand.100"
+                  rounded="lg"
+                  value={postValue.title}
+                  placeholder="Enter title of your post"
+                  onChange={handlePostValueChange}
+                />
+                <FormErrorMessage>Your First name is invalid</FormErrorMessage>
+                <FormLabel mt={5}>Description</FormLabel>
+                <Textarea
+                  name='description'
+                  rounded="lg"
+                  focusBorderColor="brand.100"
+                  colorScheme="brand"
+                  rows={10}
+                  _placeholder={{ fontSize: 18 }}
+                  value={postValue.description}
+                  onChange={handlePostValueChange}
+                  placeholder="Enter description of your post"
+                  size="sm"
+                />
+              </FormControl>
+            </Box>
+          </ModalBody>
+          <ModalFooter>
+            <Button
+              color="white"
+              colorScheme="ghost"
+              mr={3}
+              onClick={modalClose}
+            >
+              Cancel
+            </Button>
+            <Button
+              rounded="full"
+              color="white"
+              colorScheme="brand"
+              variant="solid"
+            >
+              Create
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </div>
   );
 };
