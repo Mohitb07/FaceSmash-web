fc3a19f663b879d70ef4a6e574e1dbe790755bb6
Mohitb07 <bmohit980@gmail.com>
2022-12-17 21:09:08 +0530

    refactor: post modal component and search drawer component added

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -5,65 +5,29 @@ import { BsSearch } from 'react-icons/bs';
 import { RxHamburgerMenu } from 'react-icons/rx';
 import { HiOutlinePlusCircle } from 'react-icons/hi';
 import Link from 'next/link';
-import {
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
-  Button,
-  Input,
-  Box,
-  FormControl,
-  FormLabel,
-  FormErrorMessage,
-  Textarea,
-} from '@chakra-ui/react';
-import {
-  Drawer,
-  DrawerBody,
-  DrawerHeader,
-  DrawerOverlay,
-  DrawerContent,
-} from '@chakra-ui/react';
-import {
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalFooter,
-  ModalBody,
-  ModalCloseButton,
-} from '@chakra-ui/react';
+import { Menu, MenuButton, MenuList, MenuItem } from '@chakra-ui/react';
 
 import Avatar from '../Avatar';
 import Brand from '../Brand';
-import User from '../User';
+
 import NavItem from '../NavItem';
+import PostModal from './PostModal';
+import SearchDrawer from './SearchDrawer';
 
 const Sidebar = () => {
   const [isSearchDrawerOpen, setIsSearchDrawerOpen] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [postValue, setPostValue] = useState({
-    title: '',
-    description: '',
-  });
 
   const searchDrawerClose = () => {
     if (isSearchDrawerOpen) {
       setIsSearchDrawerOpen(false);
     }
   };
+
   const modalClose = () => {
     if (isModalOpen) {
       setIsModalOpen(false);
     }
-  };
-
-  const handlePostValueChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    setPostValue(prev => ({
-      ...prev,
-      [e.target.name]: e.target.value
-    }))
   };
 
   return (
@@ -126,90 +90,11 @@ const Sidebar = () => {
           </MenuList>
         </Menu>
       </div>
-      <Drawer
-        placement="left"
-        onClose={searchDrawerClose}
-        isOpen={isSearchDrawerOpen}
-        size="md"
-      >
-        <DrawerOverlay />
-        <DrawerContent>
-          <DrawerHeader borderBottomWidth="1px">
-            <h2 className="text-3xl">Search</h2>
-            <div>
-              <div className="flex w-full py-3 px-2 my-5 items-center bg-gray-800 rounded-md">
-                <BsSearch className="text-xl text-slate-300 mx-3" />
-                <input
-                  className="w-full bg-transparent outline-none border-none"
-                  type="text"
-                  placeholder="Search User..."
-                />
-              </div>
-            </div>
-          </DrawerHeader>
-          <DrawerBody>
-            <p className="text-xl">Recent</p>
-            <User />
-            <User />
-            <User />
-          </DrawerBody>
-        </DrawerContent>
-      </Drawer>
-      <Modal isOpen={isModalOpen} onClose={modalClose} size="4xl">
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Create new post</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            <Box p={1}>
-              <FormControl isRequired isInvalid={false}>
-                <FormLabel>Title</FormLabel>
-                <Input
-                  autoFocus
-                  name='title'
-                  focusBorderColor="brand.100"
-                  rounded="lg"
-                  value={postValue.title}
-                  placeholder="Enter title of your post"
-                  onChange={handlePostValueChange}
-                />
-                <FormErrorMessage>Your First name is invalid</FormErrorMessage>
-                <FormLabel mt={5}>Description</FormLabel>
-                <Textarea
-                  name='description'
-                  rounded="lg"
-                  focusBorderColor="brand.100"
-                  colorScheme="brand"
-                  rows={10}
-                  _placeholder={{ fontSize: 18 }}
-                  value={postValue.description}
-                  onChange={handlePostValueChange}
-                  placeholder="Enter description of your post"
-                  size="sm"
-                />
-              </FormControl>
-            </Box>
-          </ModalBody>
-          <ModalFooter>
-            <Button
-              color="white"
-              colorScheme="ghost"
-              mr={3}
-              onClick={modalClose}
-            >
-              Cancel
-            </Button>
-            <Button
-              rounded="full"
-              color="white"
-              colorScheme="brand"
-              variant="solid"
-            >
-              Create
-            </Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
+      <SearchDrawer
+        isSearchDrawerOpen={isSearchDrawerOpen}
+        searchDrawerClose={searchDrawerClose}
+      />
+      <PostModal isModalOpen={isModalOpen} modalClose={modalClose} />
     </div>
   );
 };
