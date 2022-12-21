e2fc7e5d65249dee15a39d611b6047edc25d8311
Mohitb07 <bmohit980@gmail.com>
2022-12-21 20:49:14 +0530

    feat: active state added to bottom navigation links and post modal added

--- src/components/BottomNavigation/index.tsx
+++ src/components/BottomNavigation/index.tsx
@@ -1,33 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { Avatar } from '@chakra-ui/react';
 import { BsSearch } from 'react-icons/bs';
 import { HiOutlinePlusCircle } from 'react-icons/hi';
 import { TiHome } from 'react-icons/ti';
-import Avatar from '../Avatar';
+import { VscHome } from 'react-icons/vsc';
+import PostModal from '../Sidebar/PostModal';
 
-type BottomNavigationProps = {
-    
+const BottomNavigation = () => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+  const router = useRouter();
+
+  const modalClose = () => {
+    if (isModalOpen) {
+      setIsModalOpen(false);
+    }
+  };
+
+  return (
+    <div className="grid grid-cols-4 p-2 place-items-center md:hidden h-14 w-full">
+      <div>
+        <Link href="/">
+          {router.pathname === '/' ? (
+            <TiHome className="text-2xl" />
+          ) : (
+            <VscHome className="text-2xl" />
+          )}
+        </Link>
+      </div>
+      <div>
+        <BsSearch className="text-xl" />
+      </div>
+      <div>
+        <HiOutlinePlusCircle onClick={() => setIsModalOpen(true)} className="text-2xl" />
+        <PostModal isModalOpen={isModalOpen} modalClose={modalClose}/>
+      </div>
+      <div>
+        <Link href="/mohitbisht1903">
+          <Avatar
+            ring={router.pathname === '/[username]' ? 2 : 0}
+            ringColor="white"
+            className="hover-animation"
+            size="xs"
+            name="Mohit Bisht"
+            src="https://lh3.googleusercontent.com/ogw/AOh-ky2wAgtbl4h_XUEs5x-5xfgBLXa_Aq0k6ahwaOxCgw=s32-c-mo"
+          />
+        </Link>
+      </div>
+    </div>
+  );
 };
-
-const BottomNavigation:React.FC<BottomNavigationProps> = () => {
-    return (
-        <div className='grid grid-cols-4 p-2 place-items-center md:hidden h-14 w-full'>
-            <div>
-                <TiHome className="text-2xl" />
-            </div>
-            <div>
-                <BsSearch className="text-xl" />
-            </div>
-            <div>
-                <HiOutlinePlusCircle className='text-2xl'/>
-            </div>
-            <div>
-            <Avatar
-              url={
-                'https://lh3.googleusercontent.com/ogw/AOh-ky2wAgtbl4h_XUEs5x-5xfgBLXa_Aq0k6ahwaOxCgw=s32-c-mo'
-              }
-            />
-            </div>
-        </div>
-    )
-}
 export default BottomNavigation;
