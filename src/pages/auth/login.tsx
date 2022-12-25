7af997a61db201cd1f267259bc2f967fb9c99129
Mohitb07 <bmohit980@gmail.com>
2022-12-25 22:21:46 +0530

    feat: login funtionality added init

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {AuthError} from 'firebase/auth'
+import { useAuthSignInWithEmailAndPassword } from "@react-query-firebase/auth";
+import { Button } from '@chakra-ui/react';
+
 import AuthLayout from '../../components/Auth/Layout';
-import Button from '../../components/Button';
 import Input from '../../components/Input';
+import { auth } from '../../../firebase';
+import { FIREBASE_ERRORS } from '../../../firebase/error';
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<AuthError>();
+
+  async function onSignIn() {
+    try {
+      setLoading(true);
+      mutation.mutate({ email, password });
+    } catch (e) {
+      console.log('error', e)
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  const mutation = useAuthSignInWithEmailAndPassword(auth, {
+    onError(error) {
+      setError(error);
+    },
+  });
+  
+
+  console.log('error found', error?.code)
+  const disableLoginBtn = loading || !email || !password
+  
   return (
     <AuthLayout
       meta="Login"
@@ -12,16 +43,34 @@ const Login = () => {
       footerLink="/auth/register"
     >
       <div className="mt-5 flex flex-col space-y-2">
-        <div className='flex flex-col space-y-1'>
-          <label htmlFor="" className='text-[#8E8FAB]'>Email</label>
-          <Input type="email" placeholder="Email" />
+        <div className="flex flex-col space-y-1">
+          <label htmlFor="" className="text-[#8E8FAB]">
+            Email
+          </label>
+          <Input
+            type="email"
+            placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
+            isError={Boolean(error?.message)}
+            errorText={FIREBASE_ERRORS[error?.message as keyof typeof FIREBASE_ERRORS]}
+          />
         </div>
 
-        <div className='flex flex-col space-y-1 pb-10'>
-          <label htmlFor="" className='text-[#8E8FAB]'>Password</label>
-          <Input type="password" placeholder="Password" />
+        <div className="flex flex-col space-y-1 pb-10">
+          <label htmlFor="" className="text-[#8E8FAB]">
+            Password
+          </label>
+          <Input
+            type="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+            isError={Boolean(error?.message)}
+            errorText={FIREBASE_ERRORS[error?.message as keyof typeof FIREBASE_ERRORS]}
+          />
         </div>
-        <Button onClick={() => {}} label="Log in" />
+        <Button isLoading={loading} color="white" colorScheme="brand" size="md" isDisabled={disableLoginBtn} onClick={onSignIn}>
+          Log in
+        </Button>
       </div>
     </AuthLayout>
   );
