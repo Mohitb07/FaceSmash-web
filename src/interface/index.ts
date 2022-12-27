daebbf7d0ce8a3fed3646a59f825cede705fe25d
Mohitb07 <bmohit980@gmail.com>
2022-12-27 22:09:24 +0530

    feat: typescript interface added

---
+++ src/interface/index.ts
@@ -0,0 +1,10 @@
+export interface IUserDetail {
+    createdAt: string
+    email: string
+    lastSignIn: string
+    profilePic: string
+    qusername: string
+    uid: string
+    username: string
+    bio?: string
+  }
