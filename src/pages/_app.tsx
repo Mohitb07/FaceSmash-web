0115c1ebc10faf908ffb2a0fdbd752cac0c397d2
Mohitb07 <bmohit980@gmail.com>
2022-12-27 22:12:29 +0530

    feat: auth user context providers added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
-import '../../styles/globals.css';
-import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
+import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from 'react-query';
-
-import Sidebar from '../components/Sidebar';
+import '../../styles/globals.css';
 import BottomNavigation from '../components/BottomNavigation';
+import Sidebar from '../components/Sidebar';
+
+import AuthUserProvider from '../context/authUser';
+import UserDataProvider from '../context/user';
 import { theme } from '../theme';
 
 const queryClient = new QueryClient();
@@ -12,17 +14,21 @@ const queryClient = new QueryClient();
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
-      <ChakraProvider theme={theme}>
-        <div className="relative">
-          <div>
-            <Sidebar />
-          </div>
-          <div className="bg-slate-900 fixed z-50 bottom-0 w-full">
-            <BottomNavigation />
-          </div>
-          <Component {...pageProps} />
-        </div>
-      </ChakraProvider>
+      <AuthUserProvider>
+        <UserDataProvider>
+          <ChakraProvider theme={theme}>
+            <div className="relative">
+              <div>
+                <Sidebar />
+              </div>
+              <div className="bg-slate-900 fixed z-50 bottom-0 w-full">
+                <BottomNavigation />
+              </div>
+              <Component {...pageProps} />
+            </div>
+          </ChakraProvider>
+        </UserDataProvider>
+      </AuthUserProvider>
     </QueryClientProvider>
   );
 }
