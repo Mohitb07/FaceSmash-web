aece4850f1830753bb0111b80bfaed40fac1ac9a
Mohitb07 <bmohit980@gmail.com>
2022-12-27 22:06:57 +0530

    refactor: firebase host provided

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['','static.cdninstagram.com', 'lh3.googleusercontent.com', 'scontent-del1-2.cdninstagram.com', 'projects.websetters.in'],
+    domains: ['','static.cdninstagram.com', 'lh3.googleusercontent.com', 'scontent-del1-2.cdninstagram.com', 'projects.websetters.in', 'firebasestorage.googleapis.com'],
   },
 };
 
