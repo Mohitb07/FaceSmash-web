14d8a88c77525e75e312178c2bef7bdd133e790a
Mohitb07 <bmohit980@gmail.com>
2023-01-04 21:08:16 +0530

    refactor: navigations moved to respective private routes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,16 @@
 import { ChakraProvider } from '@chakra-ui/react';
+
 import type { AppProps } from 'next/app';
 import '../../styles/globals.css';
-import BottomNavigation from '../components/BottomNavigation';
-import Sidebar from '../components/Sidebar';
-
 import UserDataProvider from '../context/authUser';
 import { theme } from '../theme';
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <UserDataProvider>
       <ChakraProvider theme={theme}>
         <div className="relative">
-          <div>
-            <Sidebar />
-          </div>
-          <div className="bg-slate-900 fixed z-50 bottom-0 w-full">
-            <BottomNavigation />
-          </div>
           <Component {...pageProps} />
         </div>
       </ChakraProvider>
