c8a1a4b1cc87c209b2e3a41d83e0f092d9ef5b92
Mohitb07 <bmohit980@gmail.com>
2023-01-04 21:06:02 +0530

    feat: login custom hook added

---
+++ src/hooks/useLogin.ts
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { NextRouter } from 'next/router';
+
+const DEFAULT_ERROR_VALUE = {
+  email: '',
+  password: '',
+};
+
+export const useLogin = (router: NextRouter) => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(DEFAULT_ERROR_VALUE);
+
+  function onSignIn(email: string, password: string) {
+    setLoading(true);
+    setError(DEFAULT_ERROR_VALUE);
+    const auth = getAuth();
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        console.log('user logged in');
+        router.replace('/');
+      })
+      .catch((error) => {
+        console.log('error', error);
+        if (error.code === 'auth/invalid-email') {
+          setError((prev) => ({
+            ...prev,
+            email: error.message,
+          }));
+        } else if (
+          error.code === 'auth/user-not-found' ||
+          error.code === 'auth/wrong-password'
+        ) {
+          setError({
+            email: error.message,
+            password: error.message,
+          });
+        } else {
+          console.log('SOME OTHER ERROR', error);
+        }
+      })
+      .finally(() => setLoading(false));
+  }
+
+  return { loading, error, onSignIn, setError };
+};
