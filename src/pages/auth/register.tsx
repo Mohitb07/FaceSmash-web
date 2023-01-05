32d5f9f6172a6e2100f6c0dffe03988dd85c9d56
Mohitb07 <bmohit980@gmail.com>
2023-01-05 22:46:21 +0530

    feat: register functionality init

--- src/pages/auth/register.tsx
+++ src/pages/auth/register.tsx
@@ -1,10 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AuthLayout from '../../components/Auth/Layout';
-import Button from '../../components/Button';
-import Input from '../../components/Input';
+
+import {
+  Input,
+  Button,
+  InputGroup,
+  InputRightElement,
+  Text,
+  useBoolean,
+} from '@chakra-ui/react';
+
 import { withPublic } from '../../routes/WithPublic';
+import { FIREBASE_ERRORS } from '../../../firebase/error';
+import { useRouter } from 'next/router';
+import { useRegister } from '../../hooks/useRegister';
 
 const Register = () => {
+  const router = useRouter();
+  const [registerFieldData, setRegisterFieldData] = useState({
+    username: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+  const [charactersLeft, setCharactersLeft] = useState(30);
+  const [isShown, setIsShown] = useBoolean(false);
+  const { loading, error, onSignUp, setError } = useRegister(router);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRegisterFieldData((prev) => ({
+      ...prev,
+      [event.target.name]: event.target.value,
+    }));
+  };
+
+  const handleUsername = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.value.length > 30) {
+      return;
+    }
+    setRegisterFieldData((prev) => ({
+      ...prev,
+      username: event.target.value,
+    }));
+    setCharactersLeft(30 - event.target.value.length);
+  };
+
+  const isPasswordMatch =
+    registerFieldData.password === registerFieldData.confirmPassword;
+  const confirmPasswordLabel = "Password doesn't match";
+
   return (
     <AuthLayout
       meta="Sign Up"
@@ -15,32 +59,138 @@ const Register = () => {
     >
       <div className="mt-5 flex flex-col space-y-2">
         <div className="flex flex-col space-y-1">
-          <label htmlFor="" className="text-[#8E8FAB]">
+          <label htmlFor="username" className="text-[#8E8FAB]">
             Username
           </label>
-          <Input placeholder="Username" />
+          <Input
+            isDisabled={loading}
+            value={registerFieldData.username}
+            onChange={handleUsername}
+            errorBorderColor="crimson"
+            isInvalid={charactersLeft === 0}
+            focusBorderColor="brand.100"
+            id="username"
+            name="username"
+            size="lg"
+            colorScheme="brand"
+            variant="filled"
+            placeholder="Enter your username"
+            type="text"
+          />
+          <Text
+            fontSize="medium"
+            color={charactersLeft > 0 ? 'white' : 'crimson'}
+          >
+            Characters left {charactersLeft}
+          </Text>
         </div>
         <div className="flex flex-col space-y-1">
-          <label htmlFor="" className="text-[#8E8FAB]">
+          <label htmlFor="email" className="text-[#8E8FAB]">
             Email
           </label>
-          <Input type="email" placeholder="Email" />
+          <Input
+            isDisabled={loading}
+            value={registerFieldData.email}
+            onChange={handleChange}
+            errorBorderColor="crimson"
+            isInvalid={Boolean(error.email)}
+            focusBorderColor="brand.100"
+            id="email"
+            name="email"
+            size="lg"
+            colorScheme="brand"
+            variant="filled"
+            placeholder="Email"
+            type="email"
+          />
+          {error.email && (
+            <Text fontSize="medium" color="crimson">
+              {FIREBASE_ERRORS[error.email as keyof typeof FIREBASE_ERRORS]}
+            </Text>
+          )}
         </div>
         <div className="flex flex-col space-y-1">
-          <label htmlFor="" className="text-[#8E8FAB]">
+          <label htmlFor="password" className="text-[#8E8FAB]">
             Password
           </label>
-          <Input type="password" placeholder="Password" />
+          <InputGroup size="lg">
+            <Input
+              isDisabled={loading}
+              value={registerFieldData.password}
+              errorBorderColor="crimson"
+              isInvalid={Boolean(error.password)}
+              focusBorderColor="brand.100"
+              id="password"
+              name="password"
+              size="lg"
+              colorScheme="brand"
+              variant="filled"
+              placeholder="Password"
+              type={isShown ? 'text' : 'password'}
+              onChange={handleChange}
+            />
+            {registerFieldData.password.length > 0 && (
+              <InputRightElement width="4.5rem">
+                <Button h="1.75rem" size="sm" onClick={setIsShown.toggle}>
+                  {isShown ? 'Hide' : 'Show'}
+                </Button>
+              </InputRightElement>
+            )}
+          </InputGroup>
+          {error.password && (
+            <Text fontSize="medium" color="crimson">
+              {FIREBASE_ERRORS[error.password as keyof typeof FIREBASE_ERRORS]}
+            </Text>
+          )}
         </div>
         <div className="flex flex-col space-y-1 pb-10">
-          <label htmlFor="" className="text-[#8E8FAB]">
+          <label htmlFor="password" className="text-[#8E8FAB]">
             Confirm Password
           </label>
-          <Input type="password" placeholder="Confirm Password" />
+          <InputGroup size="lg">
+            <Input
+              isDisabled={loading}
+              value={registerFieldData.confirmPassword}
+              errorBorderColor="crimson"
+              isInvalid={
+                registerFieldData.confirmPassword.length > 0 && !isPasswordMatch
+              }
+              focusBorderColor="brand.100"
+              id="confirmPassword"
+              name="confirmPassword"
+              size="lg"
+              colorScheme="brand"
+              variant="filled"
+              placeholder="Re-Enter your password"
+              type={isShown ? 'text' : 'password'}
+              onChange={handleChange}
+            />
+            {registerFieldData.confirmPassword.length > 0 && (
+              <InputRightElement width="4.5rem">
+                <Button h="1.75rem" size="sm" onClick={setIsShown.toggle}>
+                  {isShown ? 'Hide' : 'Show'}
+                </Button>
+              </InputRightElement>
+            )}
+          </InputGroup>
+          {registerFieldData.confirmPassword.length > 0 && !isPasswordMatch && (
+            <Text fontSize="medium" color="crimson">
+              {confirmPasswordLabel}
+            </Text>
+          )}
         </div>
-        <Button onClick={() => {}} label="Sign Up"/>{' '}
+        <Button
+          isLoading={false}
+          color="white"
+          colorScheme="brand"
+          size="md"
+          isDisabled={false}
+          onClick={() => {}}
+        >
+          Sign Up
+        </Button>
       </div>
     </AuthLayout>
   );
 };
-export default withPublic(Register)
+export default withPublic(Register);
