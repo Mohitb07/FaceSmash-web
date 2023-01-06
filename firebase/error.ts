8631aa6468ac7ed877de07f4e4e39c63a08d0a1e
Mohitb07 <bmohit980@gmail.com>
2023-01-06 22:08:54 +0530

    refactor: correct firebase keys provided

--- firebase/error.ts
+++ firebase/error.ts
@@ -1,16 +1,13 @@
 export const FIREBASE_ERRORS = {
-    '[auth/email-already-in-use] The email address is already in use by another account.':
-      'A user with that email already exists',
-  
-    'Firebase: Error (auth/user-not-found).':
-      'Invalid Email or Password',
-  
-    'Firebase: Error (auth/wrong-password).':
-      'Invalid email or password',
-  
-    'Firebase: Error (auth/invalid-email).': 'Invalid email',
-  
-    '[auth/weak-password] The given password is invalid. [ Password should be at least 6 characters ]':
-      'Password should be at least 6 characters',
-  }
-  
+  'Firebase: Error (auth/email-already-in-use).':
+    'A user with that email already exists',
+
+  'Firebase: Error (auth/user-not-found).': 'Invalid Email or Password',
+
+  'Firebase: Error (auth/wrong-password).': 'Invalid email or password',
+
+  'Firebase: Error (auth/invalid-email).': 'Invalid email',
+
+  'Firebase: Password should be at least 6 characters (auth/weak-password).':
+    'Password should be at least 6 characters long',
+};
