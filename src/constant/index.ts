01dd10063349194ce5a8a31691f5a1a515e66c88
Mohitb07 <bmohit980@gmail.com>
2023-01-06 22:04:18 +0530

    feat: constant values added

---
+++ src/constant/index.ts
@@ -0,0 +1,8 @@
+export const DEFAULT_PROFILE_PIC =
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTG3eLpTAMWO-mtILepXLwg68-IChyGcXJgog&usqp=CAU';
+
+export const FEED_LIMIT = 5;
+export const USERS_LIMIT = 5;
+
+export const USERS_COLLECTION = 'Users';
+export const POSTS_COLLECTION = 'Posts';
