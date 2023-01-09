410318f7263e2d97673cb8b48742ba2d006812f7
Mohitb07 <bmohit980@gmail.com>
2023-01-09 22:06:19 +0530

    refactor: loading state and real data mapped

--- src/components/DataList/index.tsx
+++ src/components/DataList/index.tsx
@@ -1,17 +1,57 @@
+import { Skeleton, SkeletonCircle, SkeletonText } from '@chakra-ui/react';
 import React from 'react';
+import { Post } from '../../interface';
 import Feed from '../Feed';
 
-const VirtualisedList = () => {
+type PostListProps = {
+  list: Post[];
+  isLoading: boolean;
+};
+
+const PostList = ({ list, isLoading }: PostListProps) => {
+  if (isLoading) {
+    return (
+      <div className="md:w-[500px] space-y-5 lg:w-[450px] xl:w-[600px] rounded-md">
+        <div className="space-y-2">
+          <SkeletonCircle height="14" width="14" />
+          <Skeleton height="500px"></Skeleton>
+          <SkeletonText spacing="4" />
+        </div>
+        <div className="space-y-2">
+          <SkeletonCircle height="14" width="14" />
+          <Skeleton height="500px"></Skeleton>
+          <SkeletonText spacing="4" />
+        </div>
+        <div className="space-y-2">
+          <SkeletonCircle height="14" width="14" />
+          <Skeleton height="500px"></Skeleton>
+          <SkeletonText spacing="4" />
+        </div>
+      </div>
+    );
+  }
   return (
     <>
-      <Feed />
-      <Feed />
-      <Feed />
-      <Feed />
+      {list.map((feed) => (
+        <Feed
+          key={feed.key}
+          username={feed.username}
+          postImage={feed.image}
+          userProfile={feed.userProfile}
+          createdAt={feed.createdAt}
+          description={feed.description}
+          link={feed.link}
+          imageRef={feed.imageRef}
+          likes={feed.likes}
+          userId={feed.user}
+          postTitle={feed.title}
+          postId={feed.key}
+        />
+      ))}
       <>
         <p className="text-center text-gray-500">End Of Result</p>
       </>
     </>
   );
 };
-export default VirtualisedList;
+export default PostList;
