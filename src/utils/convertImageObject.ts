a4272abe3450013d6f51f9b5696f15f212c6b6a6
Mohitb07 <bmohit980@gmail.com>
2023-01-09 22:04:51 +0530

    feat: image to blob convertor

---
+++ src/utils/convertImageObject.ts
@@ -0,0 +1,6 @@
+export const convertImageObject = (e: React.ChangeEvent<HTMLInputElement>) => {
+  if(e.target.files && e.target.files[0]){
+    return URL.createObjectURL(e.target.files![0]);
+  }
+  throw new Error('image cannot be converted')
+};
