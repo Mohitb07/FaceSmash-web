32407d0270e37c046d896dd21710441245e95158
Mohitb07 <bmohit980@gmail.com>
2023-01-09 22:04:14 +0530

    feat: interfaces added

--- src/interface/index.ts
+++ src/interface/index.ts
@@ -1,3 +1,5 @@
+import {Timestamp} from 'firebase/firestore'
+
 export interface IUserDetail {
     createdAt: string
     email: string
@@ -7,4 +9,32 @@ export interface IUserDetail {
     uid: string
     username: string
     bio?: string
+  }
+
+  export interface Post {
+    createdAt: Timestamp | null
+    description: string
+    key: string
+    image?: string
+    likes: number
+    link?: string
+    title: string
+    user: string
+    userProfile: string
+    username: string
+    imageRef?: string
+  }
+
+  export interface FeedProps {
+    postImage?: string
+    postTitle: string
+    username: string
+    userProfile: string
+    description: string
+    createdAt: Timestamp | null
+    postId: string
+    userId: string
+    link?: string
+    imageRef?: string
+    likes: number
   }
