b0d02204976365a2b56f282a008ba16ac3455638
Mohitb07 <bmohit980@gmail.com>
2023-01-13 22:11:46 +0530

    refactor: code refactored

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,20 +11,42 @@ import { Main } from '../templates/Main';
 import Navigation from '../common/Navigation';
 import { Post } from '../interface';
 import { useGetPosts } from '../hooks/useGetPosts';
+import EmptyData from '../components/DataList/EmptyData';
+import Footer from '../components/DataList/Footer';
+import Feed from '../components/Feed';
 
 function Home() {
   const [feedList, setFeedList] = useState<Post[]>([]);
-  const {getPosts, isLoading} = useGetPosts()
+  const { getPosts, isLoading } = useGetPosts();
 
   useEffect(() => {
     const getAllPosts = async () => {
-      const posts = await getPosts()
-      setFeedList(posts)
-    }
+      const posts = await getPosts();
+      setFeedList(posts);
+    };
     getAllPosts();
   }, [getPosts]);
 
   console.log('posts list', feedList);
+
+  function renderItem<T extends Post>(feed: T) {
+    return (
+      <Feed
+        key={feed.key}
+        username={feed.username}
+        postImage={feed.image}
+        userProfile={feed.userProfile}
+        createdAt={feed.createdAt}
+        description={feed.description}
+        link={feed.link}
+        imageRef={feed.imageRef}
+        likes={feed.likes}
+        userId={feed.user}
+        postTitle={feed.title}
+        postId={feed.key}
+      />
+    );
+  }
 
   return (
     <Main
@@ -36,31 +58,37 @@ function Home() {
       }
     >
       <Navigation />
-        <div className="h-screen">
-          <div>
-            <header className="block md:hidden text-center">
-              <Brand />
-            </header>
-            <div className="px-5">
-              <div className="flex w-full md:hidden py-4 px-2 my-5 items-center bg-gray-800 rounded-md">
-                <BsSearch className="text-xl text-slate-300 mx-3" />
-                <input
-                  className="w-full bg-transparent outline-none border-none"
-                  type="text"
-                  placeholder="Search User..."
-                />
-              </div>
-            </div>
-            <div className="flex md:p-10 justify-center items-start gap-10">
-              <main className="space-y-5 pb-16 md:ml-[20%] xl:ml-[10%]">
-                <DataList list={feedList} isLoading={isLoading}/>
-              </main>
-              <aside className="hidden lg:flex flex-col">
-                <UserRecommendation />
-              </aside>
+      <div className="h-screen">
+        <div>
+          <header className="block md:hidden text-center">
+            <Brand />
+          </header>
+          <div className="px-5">
+            <div className="flex w-full md:hidden py-4 px-2 my-5 items-center bg-gray-800 rounded-md">
+              <BsSearch className="text-xl text-slate-300 mx-3" />
+              <input
+                className="w-full bg-transparent outline-none border-none"
+                type="text"
+                placeholder="Search User..."
+              />
             </div>
           </div>
+          <div className="flex md:p-10 justify-center items-start gap-10">
+            <main className="space-y-5 pb-16 md:ml-[20%] xl:ml-[10%]">
+              <DataList
+                ListEmptyComponent={EmptyData}
+                ListFooterComponent={Footer}
+                data={feedList}
+                isLoading={isLoading}
+                renderItem={(item: any) => renderItem(item)}
+              />
+            </main>
+            <aside className="hidden lg:flex flex-col">
+              <UserRecommendation />
+            </aside>
+          </div>
         </div>
+      </div>
     </Main>
   );
 }
