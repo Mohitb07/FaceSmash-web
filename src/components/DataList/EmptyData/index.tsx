576e94943c6d1c5f4bfc34e997b8cd17886ad8a1
Mohitb07 <bmohit980@gmail.com>
2023-01-13 22:10:40 +0530

    feat: component added

---
+++ src/components/DataList/EmptyData/index.tsx
@@ -0,0 +1,11 @@
+import { Box, Text } from '@chakra-ui/react';
+import React from 'react';
+
+const EmptyData = () => {
+    return (
+        <Box>
+            <Text>Empty List</Text>
+        </Box>
+    )
+}
+export default EmptyData;
---
+++ src/components/DataList/Footer/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <>
+      <p className="text-center text-gray-500">End Of Result</p>
+    </>
+  );
+};
+export default Footer;
