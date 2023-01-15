83862c305c98b393c4185b100761c28962661aec
Mohitb07 <bmohit980@gmail.com>
2023-01-15 22:15:36 +0530

    refactor: code refactored

--- src/components/User/index.tsx
+++ src/components/User/index.tsx
@@ -3,32 +3,33 @@ import Avatar from '../Avatar';
 
 type UserProps = {
   size?: 'small' | 'large';
+  profileURL: string;
+  username: string;
+  email: string;
 };
 
-const User = ({ size = 'small' }: UserProps) => {
-  let height,width, textSize;
-  if(size === 'small'){
-    height = 50
-    width = 50
-    textSize = 'lg'
+const User = ({ size = 'small', profileURL, username, email }: UserProps) => {
+  let height, width, textSize;
+  if (size === 'small') {
+    height = 50;
+    width = 50;
+    textSize = 'lg';
   }
-  if(size === "large"){
-    height = 70
-    width = 70
-    textSize = 'xl'
+  if (size === 'large') {
+    height = 70;
+    width = 70;
+    textSize = 'xl';
   }
   return (
     <div className="flex items-center gap-5 mt-5">
-      <Avatar
-        url={
-          'http://projects.websetters.in/digg-seos/digg/wp-content/themes/twentytwenty-child-theme/img/demo-prof.jpg'
-        }
-        height={height}
-        width={width}
-      />
+      <Avatar url={profileURL} height={height} width={width} />
       <div>
-        <p className="font-semibold tracking-wide">mohitbisht1903</p>
-        <p className={`text-${textSize} text-gray-500`}>Mohit Bisht</p>
+        <p aria-label="email" className="font-semibold tracking-wide">
+          {email}
+        </p>
+        <p aria-label="username" className={`text-${textSize} text-gray-500`}>
+          {username}
+        </p>
       </div>
     </div>
   );
