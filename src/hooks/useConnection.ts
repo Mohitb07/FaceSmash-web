10fb3a0c8f3bc843e052a2b811bd1a4a92803071
Mohitb07 <bmohit980@gmail.com>
2023-01-20 22:11:47 +0530

    feat: custom hook for connections related data added

---
+++ src/hooks/useConnection.ts
@@ -0,0 +1,66 @@
+import {
+  collection,
+  getDoc,
+  onSnapshot,
+  query,
+  Unsubscribe,
+} from 'firebase/firestore';
+import { useEffect, useState } from 'react';
+import { db } from '../../firebase';
+import { USERS_COLLECTION } from '../constant';
+import { User } from '../interface';
+
+export const useConnection = (userId: string) => {
+  const [followersList, setFollowersList] = useState<User[]>([]);
+  const [followingList, setFollowingList] = useState<User[]>([]);
+  const [connectionsCount, setConnectionCount] = useState({
+    following: 0,
+    followers: 0,
+  });
+
+  useEffect(() => {
+    let unsubscribeFollowingData: Unsubscribe;
+    let unsubscribeFollowersData: Unsubscribe;
+    async function getData() {
+      const followingSubColRef = query(
+        collection(db, `${USERS_COLLECTION}/${userId}/followings/`)
+      );
+      unsubscribeFollowingData = onSnapshot(
+        followingSubColRef,
+        async (querySnap) => {
+          const promises = querySnap.docs.map((d) => getDoc(d.data().user));
+          const result = await Promise.all(promises);
+          const list = result.map((d) => d.data() as User);
+          setFollowingList(list);
+          setConnectionCount(prev => ({
+            ...prev,
+            following: querySnap.size
+          }))
+        }
+      );
+
+      const followersSubColRef = query(
+        collection(db, `${USERS_COLLECTION}/${userId}/followers/`)
+      );
+      unsubscribeFollowersData = onSnapshot(
+        followersSubColRef,
+        async (querySnap) => {
+          const promises = querySnap.docs.map((d) => getDoc(d.data().user));
+          const result = await Promise.all(promises);
+          const list = result.map((d) => d.data() as User);
+          setFollowersList(list);
+          setConnectionCount(prev => ({
+            ...prev,
+            followers: querySnap.size
+          }))
+        }
+      );
+    }
+    getData();
+    return () => {
+      unsubscribeFollowingData(), unsubscribeFollowersData();
+    };
+  }, [userId]);
+
+  return {connectionsCount, followersList, followingList}
+};
