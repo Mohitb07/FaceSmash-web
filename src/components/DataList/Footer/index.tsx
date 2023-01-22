c127bca4c798f418670265e4f4ab1d4de569e65a
Mohitb07 <bmohit980@gmail.com>
2023-01-22 21:44:11 +0530

    refactor: loading state added

--- src/components/DataList/Footer/index.tsx
+++ src/components/DataList/Footer/index.tsx
@@ -1,10 +1,42 @@
+import { Skeleton, SkeletonCircle, SkeletonText, Text } from '@chakra-ui/react';
 import React from 'react';
 
-const Footer = () => {
-  return (
-    <>
-      <p className="text-center text-gray-500">End Of Result</p>
-    </>
-  );
+type FooterProps = {
+  dataList: any[];
+  loading: boolean;
+};
+
+const Footer = ({ dataList = [], loading }: FooterProps) => {
+  let content;
+  if (loading) {
+    content = (
+      <div className="md:w-[500px] space-y-5 lg:w-[450px] xl:w-[600px] rounded-md">
+        <div className="space-y-2">
+          <SkeletonCircle height="14" width="14" />
+          <Skeleton height="500px"></Skeleton>
+          <SkeletonText spacing="4" />
+        </div>
+        <div className="space-y-2">
+          <SkeletonCircle height="14" width="14" />
+          <Skeleton height="500px"></Skeleton>
+          <SkeletonText spacing="4" />
+        </div>
+        <div className="space-y-2">
+          <SkeletonCircle height="14" width="14" />
+          <Skeleton height="500px"></Skeleton>
+          <SkeletonText spacing="4" />
+        </div>
+      </div>
+    );
+  } else if (dataList.length > 0 && !loading) {
+    content = (
+      <Text textAlign="center" color="gray.500">
+        No More post
+      </Text>
+    );
+  } else {
+    return null;
+  }
+  return <div>{content}</div>;
 };
 export default Footer;
