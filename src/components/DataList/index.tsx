f5e39960f7cb72a5f9575bd6208e764d1cb01d23
Mohitb07 <bmohit980@gmail.com>
2023-01-22 21:42:32 +0530

    feat: intersection observer added

--- src/components/DataList/index.tsx
+++ src/components/DataList/index.tsx
@@ -1,54 +1,64 @@
-import React from 'react';
-import {
-  Skeleton,
-  SkeletonCircle,
-  SkeletonText,
-} from '@chakra-ui/react';
+import React, { useEffect } from 'react';
+import { DocumentSnapshot, Unsubscribe } from 'firebase/firestore';
 
 type DataListProps<D> = {
   data: D[];
   isLoading: boolean;
   ListEmptyComponent: React.ComponentType;
-  ListFooterComponent: React.ComponentType;
+  ListFooterComponent: JSX.Element;
   renderItem: <T>(item: T) => JSX.Element;
+  getMore: () => Unsubscribe;
+  lastVisible?: DocumentSnapshot;
 };
 
-const DataList = <D extends any>({
+const DataList = <D extends { key: string }>({
   data,
   isLoading,
   ListEmptyComponent,
   ListFooterComponent,
   renderItem,
+  getMore,
+  lastVisible,
 }: DataListProps<D>) => {
-  if (isLoading) {
-    return (
-      <div className="md:w-[500px] space-y-5 lg:w-[450px] xl:w-[600px] rounded-md">
-        <div className="space-y-2">
-          <SkeletonCircle height="14" width="14" />
-          <Skeleton height="500px"></Skeleton>
-          <SkeletonText spacing="4" />
-        </div>
-        <div className="space-y-2">
-          <SkeletonCircle height="14" width="14" />
-          <Skeleton height="500px"></Skeleton>
-          <SkeletonText spacing="4" />
-        </div>
-        <div className="space-y-2">
-          <SkeletonCircle height="14" width="14" />
-          <Skeleton height="500px"></Skeleton>
-          <SkeletonText spacing="4" />
-        </div>
-      </div>
-    );
-  }
+  const lastPostId = data[data.length - 1]?.key;
+
+  useEffect(() => {
+    let unsubscribe: Unsubscribe;
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          if (lastVisible) {
+            unsubscribe = getMore();
+          }
+        }
+      });
+    });
+
+    const lastPostElement = document.getElementById(lastPostId);
+    if (lastPostElement) {
+      observer.observe(lastPostElement);
+    }
+
+    return () => {
+      observer.disconnect();
+      if (typeof unsubscribe === 'function') unsubscribe();
+    };
+  }, [lastPostId, getMore, lastVisible]);
+
   if (data.length === 0 && !isLoading) {
     return <ListEmptyComponent />;
   }
   return (
     <>
       <div className="space-y-8">
-        {data.map((item) => renderItem(item))}
-        {!isLoading && <ListFooterComponent />}
+        {data.map((item) => {
+          return (
+            <div key={item.key} id={item.key}>
+              {renderItem(item)}
+            </div>
+          );
+        })}
+        {ListFooterComponent}
       </div>
     </>
   );
