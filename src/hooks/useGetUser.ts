59836ff2264f2ca077eaf2b416f304c5583bd8ef
Mohitb07 <bmohit980@gmail.com>
2023-01-26 21:26:59 +0530

    refactor: code refactored

--- src/hooks/useGetUser.ts
+++ src/hooks/useGetUser.ts
@@ -1,11 +1,10 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
-import { doc, getDoc, onSnapshot, Unsubscribe } from 'firebase/firestore';
+import { doc, onSnapshot, Unsubscribe } from 'firebase/firestore';
 
 import { db } from '../../firebase';
 import { USERS_COLLECTION } from '../constant';
 import { User } from '../interface';
-import { useAuthUser } from './useAuthUser';
 
 const DEFAULT_USER_DETAILS: User = {
   bio: '',
@@ -21,7 +20,6 @@ const DEFAULT_USER_DETAILS: User = {
 export const useGetUser = (userId: string) => {
   const [userDetail, setUserDetail] = useState<User>(DEFAULT_USER_DETAILS);
   const [isUserDetailLoading, setIsUserDetailLoading] = useState(true);
-  const { authUser } = useAuthUser();
 
   useEffect(() => {
     let unsub: Unsubscribe;
@@ -40,6 +38,6 @@ export const useGetUser = (userId: string) => {
     }
     return () => unsub();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [userId, authUser?.uid]);
+  }, [userId]);
   return { userDetail, isUserDetailLoading };
 };
