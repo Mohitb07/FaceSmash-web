c74587aabba5fe7af557c3cd6afb66ab875d14b8
Mohitb07 <bmohit980@gmail.com>
2023-01-31 22:13:41 +0530

    feat: progress bar on route change

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,23 @@
-import type { AppProps } from 'next/app';
+import '../../styles/globals.css';
+import '../../styles/Nprogress.css';
 
 import { ChakraProvider } from '@chakra-ui/react';
+import type { AppProps } from 'next/app';
+import Router from 'next/router';
+import NProgress from 'nprogress';
 
-import '../../styles/globals.css';
 import UserDataProvider from '../context/authUser';
 import { useCheckOnlineStatus } from '../hooks/useCheckOnlineStatus';
 import { theme } from '../theme';
 
+NProgress.configure({ showSpinner: false });
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
+
 export default function App({ Component, pageProps }: AppProps) {
-  useCheckOnlineStatus()
+  useCheckOnlineStatus();
   return (
     <UserDataProvider>
       <ChakraProvider theme={theme}>
