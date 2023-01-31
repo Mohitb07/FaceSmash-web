bf36e37763be43c983ce275b1886aceceea6282e
Mohitb07 <bmohit980@gmail.com>
2023-01-31 22:11:47 +0530

    build: eslint prettier and refactored

--- next.config.js
+++ next.config.js
@@ -1,10 +1,32 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: ['','static.cdninstagram.com', 'lh3.googleusercontent.com', 'scontent-del1-2.cdninstagram.com', 'projects.websetters.in', 'firebasestorage.googleapis.com', 'encrypted-tbn0.gstatic.com'],
-  },
-};
+/* eslint-disable import/no-extraneous-dependencies */
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
 
-module.exports = nextConfig;
+module.exports = withBundleAnalyzer({
+  eslint: {
+    dirs: ['.'],
+  },
+  poweredByHeader: false,
+  trailingSlash: true,
+  swcMinify: true,
+  basePath: '',
+  // The starter code load resources from `public` folder with `router.basePath` in React components.
+  // So, the source code is "basePath-ready".
+  // You can remove `basePath` if you don't need it.
+  reactStrictMode: true,
+  images: {
+    domains: ['','static.cdninstagram.com', 'lh3.googleusercontent.com', 'scontent-del1-2.cdninstagram.com', 'projects.websetters.in', 'firebasestorage.googleapis.com', 'encrypted-tbn0.gstatic.com']
+  },
+});
+
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+//   images: {
+//     ,
+//   },
+// };
+
+// module.exports = nextConfig;
