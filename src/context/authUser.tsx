3f19a820b3b1b8fc028768c36b7e3a8d3099afe5
Mohitb07 <bmohit980@gmail.com>
2023-02-14 22:53:50 +0530

    refactor: memoized context values

--- src/context/authUser.tsx
+++ src/context/authUser.tsx
@@ -1,12 +1,13 @@
 import { Spinner } from '@chakra-ui/react';
 import type { User } from 'firebase/auth';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
-import type { Unsubscribe } from 'firebase/firestore';
+import type { FirestoreError, Unsubscribe } from 'firebase/firestore';
 import { doc, onSnapshot } from 'firebase/firestore';
 import type { Dispatch, SetStateAction } from 'react';
 import type { ReactNode } from 'react';
-import { createContext, useEffect, useState } from 'react';
+import { createContext, useEffect, useMemo, useState } from 'react';
 
+import { USERS_COLLECTION } from '@/constant';
 import type { User as IUser } from '@/interface';
 
 import { db } from '../../firebase';
@@ -16,6 +17,7 @@ type UserState = {
   loading: boolean;
   isVerified: boolean;
   setIsVerified: Dispatch<SetStateAction<boolean>>;
+  error: FirestoreError | null;
 };
 
 const DEFAULT_VALUES: UserState = {
@@ -32,6 +34,7 @@ const DEFAULT_VALUES: UserState = {
   loading: true,
   isVerified: false,
   setIsVerified() {},
+  error: null,
 };
 
 export const UserContext = createContext<UserState>(DEFAULT_VALUES);
@@ -40,39 +43,36 @@ const UserProvider = ({ children }: { children: ReactNode }) => {
   const [authUser, setAuthUser] = useState<IUser | null>(null);
   const [isVerified, setIsVerified] = useState(false);
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<FirestoreError | null>(null);
+
   const auth = getAuth();
   console.log('current status', isVerified);
 
   useEffect(() => {
     let unsubscribeUser: Unsubscribe;
-    let unsubscribeAuth: Unsubscribe;
-    try {
-      unsubscribeAuth = onAuthStateChanged(auth, (user: User | null) => {
-        if (user) {
-          console.log('isVerified', user.emailVerified);
-          const uid = user.uid;
-          unsubscribeUser = onSnapshot(
-            doc(db, 'Users', uid),
-            (d) => {
-              setAuthUser(d.data() as IUser);
-              setIsVerified(user.emailVerified);
-              setLoading(false);
-            },
-            (error) => {
-              console.log('ERROR FOUND ', error);
-              setLoading(false);
-            }
-          );
-        } else {
-          console.log('user is logged out, setting load false');
-          setAuthUser(null);
-          setLoading(false);
-        }
-      });
-    } catch (error) {
-      console.log('Error ', error);
-      setLoading(false);
-    }
+    let unsubscribeAuth = onAuthStateChanged(auth, (user: User | null) => {
+      if (user) {
+        console.log('isVerified', user.emailVerified);
+        const uid = user.uid;
+        unsubscribeUser = onSnapshot(
+          doc(db, USERS_COLLECTION, uid),
+          (d) => {
+            setAuthUser(d.data() as IUser);
+            setIsVerified(user.emailVerified);
+            setLoading(false);
+          },
+          (err) => {
+            console.log('ERROR FOUND ', error);
+            setLoading(false);
+            setError(err);
+          }
+        );
+      } else {
+        console.log('user is logged out, setting load false');
+        setAuthUser(null);
+        setLoading(false);
+      }
+    });
 
     return () => {
       if (unsubscribeAuth) {
@@ -90,14 +90,17 @@ const UserProvider = ({ children }: { children: ReactNode }) => {
       <Spinner size="xl" />
     </div>;
   }
-
-  return (
-    <UserContext.Provider
-      value={{ authUser, loading, isVerified, setIsVerified }}
-    >
-      {children}
-    </UserContext.Provider>
+  const values = useMemo(
+    () => ({
+      authUser,
+      loading,
+      isVerified,
+      setIsVerified,
+      error,
+    }),
+    [authUser, loading, isVerified, setIsVerified, error]
   );
+  return <UserContext.Provider value={values}>{children}</UserContext.Provider>;
 };
 
 export default UserProvider;
