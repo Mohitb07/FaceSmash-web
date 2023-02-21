1aa55aba5103634820b5b65c9273b185daeda894
Mohitb07 <bmohit980@gmail.com>
2023-02-21 22:00:14 +0530

    refactor: now using react-hook-form lib

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,5 +1,10 @@
 import { InputGroup, InputRightElement, useBoolean } from '@chakra-ui/react';
-import React, { useEffect, useState } from 'react';
+import { yupResolver } from '@hookform/resolvers/yup';
+import React from 'react';
+import type { SubmitHandler } from 'react-hook-form';
+import { Controller } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
 
 import Button from '@/components/Button';
 import ErrorLabel from '@/components/ErrorLabel';
@@ -8,41 +13,32 @@ import { useLogin } from '@/hooks/useLogin';
 import AuthLayout from '@/layouts/Auth';
 import { withPublic } from '@/routes/WithPublic';
 
+const schema = yup
+  .object({
+    email: yup.string().email().required(),
+    password: yup.string().required(),
+  })
+  .required();
+
+type FormInput = yup.InferType<typeof schema>;
+
 const Login = () => {
-  const [loginFieldData, setLoginFieldData] = useState({
-    email: '',
-    password: '',
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormInput>({
+    resolver: yupResolver(schema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
   });
-  const { loading, error, onSignIn, setError } = useLogin();
+  const { loading, error, onSignIn } = useLogin();
   const [isShown, setIsShown] = useBoolean(false);
 
-  useEffect(() => {
-    setError((prev) => ({
-      ...prev,
-      email: '',
-    }));
-  }, [loginFieldData.email, setError]);
-
-  useEffect(() => {
-    setError((prev) => ({
-      ...prev,
-      password: '',
-    }));
-  }, [loginFieldData.password, setError]);
-
-  const disableLoginBtn =
-    loading || !loginFieldData.email || !loginFieldData.password;
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) =>
-    setLoginFieldData((prev) => ({
-      ...prev,
-      [event.target.name]: event.target.value,
-    }));
-
-  const onSignInAttempt = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    onSignIn(loginFieldData.email, loginFieldData.password);
-  };
+  const onSignInAttempt: SubmitHandler<FormInput> = (data) =>
+    onSignIn(data.email, data.password);
 
   return (
     <AuthLayout
@@ -53,57 +49,68 @@ const Login = () => {
     >
       <form
         noValidate
-        onSubmit={onSignInAttempt}
+        onSubmit={handleSubmit(onSignInAttempt)}
         className="mt-5 flex flex-col space-y-2"
       >
         <div className="flex flex-col space-y-1">
           <label htmlFor="email" className="form-label-text">
             Email
           </label>
-          <Input
-            isDisabled={loading}
-            value={loginFieldData.email}
-            onChange={handleChange}
-            isInvalid={Boolean(error.email)}
-            id="email"
+          <Controller
             name="email"
-            placeholder="Email"
-            type="email"
+            control={control}
+            render={({ field }) => (
+              <Input
+                {...field}
+                isDisabled={loading}
+                isInvalid={
+                  Boolean(error.email) || Boolean(errors?.email?.message)
+                }
+                id="email"
+                name="email"
+                placeholder="Email"
+                type="email"
+              />
+            )}
           />
-          <ErrorLabel error={error.email} />
+          <ErrorLabel
+            error={error.email}
+            validationError={errors.email?.message}
+          />
         </div>
 
         <div className="flex flex-col space-y-1 pb-10">
           <label htmlFor="password" className="form-label-text">
             Password
           </label>
-          <InputGroup size="lg">
-            <Input
-              isDisabled={loading}
-              value={loginFieldData.password}
-              isInvalid={Boolean(error.password)}
-              id="password"
-              name="password"
-              placeholder="Password"
-              type={isShown ? 'text' : 'password'}
-              onChange={handleChange}
-            />
-            {loginFieldData.password.length > 0 && (
-              <InputRightElement width="4.5rem">
-                <Button h="1.75rem" size="sm" onClick={setIsShown.toggle}>
-                  {isShown ? 'Hide' : 'Show'}
-                </Button>
-              </InputRightElement>
+          <Controller
+            name="password"
+            control={control}
+            render={({ field }) => (
+              <InputGroup size="lg">
+                <Input
+                  {...field}
+                  isDisabled={loading}
+                  isInvalid={Boolean(error.password)}
+                  id="password"
+                  name="password"
+                  placeholder="Password"
+                  type={isShown ? 'text' : 'password'}
+                />
+                <InputRightElement width="4.5rem">
+                  <Button h="1.75rem" size="sm" onClick={setIsShown.toggle}>
+                    {isShown ? 'Hide' : 'Show'}
+                  </Button>
+                </InputRightElement>
+              </InputGroup>
             )}
-          </InputGroup>
-          <ErrorLabel error={error.password} />
+          />
+          <ErrorLabel
+            error={error.password}
+            validationError={errors.password?.message}
+          />
         </div>
-        <Button
-          type="submit"
-          isLoading={loading}
-          size="md"
-          isDisabled={disableLoginBtn}
-        >
+        <Button type="submit" isLoading={loading} size="md">
           Log in
         </Button>
       </form>
