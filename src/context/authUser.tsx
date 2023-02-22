ca9056a1ca889f78e4fba810dca8562906daa1a1
Mohitb07 <bmohit980@gmail.com>
2023-02-22 16:07:10 +0530

    fix: permission denied on re-login

--- src/context/authUser.tsx
+++ src/context/authUser.tsx
@@ -1,8 +1,8 @@
 import { Spinner } from '@chakra-ui/react';
 import type { User } from 'firebase/auth';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
-import type { FirestoreError, Unsubscribe } from 'firebase/firestore';
-import { doc, onSnapshot } from 'firebase/firestore';
+import { getDoc } from 'firebase/firestore';
+import { doc } from 'firebase/firestore';
 import type { Dispatch, SetStateAction } from 'react';
 import type { ReactNode } from 'react';
 import { createContext, useEffect, useMemo, useState } from 'react';
@@ -17,7 +17,7 @@ type UserState = {
   loading: boolean;
   isVerified: boolean;
   setIsVerified: Dispatch<SetStateAction<boolean>>;
-  error: FirestoreError | null;
+  error: string | null;
 };
 
 const DEFAULT_VALUES: UserState = {
@@ -43,50 +43,45 @@ const UserProvider = ({ children }: { children: ReactNode }) => {
   const [authUser, setAuthUser] = useState<IUser | null>(null);
   const [isVerified, setIsVerified] = useState(false);
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<FirestoreError | null>(null);
+  const [error, setError] = useState<string | null>(null);
 
   const auth = getAuth();
   console.log('current status', isVerified);
 
   useEffect(() => {
-    let unsubscribeUser: Unsubscribe;
-    let unsubscribeAuth = onAuthStateChanged(auth, (user: User | null) => {
-      if (user) {
-        console.log('isVerified', user.emailVerified);
-        const uid = user.uid;
-        unsubscribeUser = onSnapshot(
-          doc(db, USERS_COLLECTION, uid),
-          (d) => {
-            setAuthUser(d.data() as IUser);
+    let unsubscribeAuth = onAuthStateChanged(
+      auth,
+      async (user: User | null) => {
+        if (user) {
+          console.log('isVerified', user.emailVerified);
+          try {
+            const userData = await getDoc(doc(db, USERS_COLLECTION, user.uid));
+            setAuthUser(userData.data() as IUser);
             setIsVerified(user.emailVerified);
             setLoading(false);
-          },
-          (err) => {
-            console.log('ERROR FOUND ', error);
+          } catch (err) {
+            console.log('Auth user error', error);
             setLoading(false);
-            setError(err);
+            setError(`Some Error ${err}`);
           }
-        );
-      } else {
-        console.log('user is logged out, setting load false');
-        setAuthUser(null);
-        setLoading(false);
+        } else {
+          console.log('user is logged out, setting load false');
+          setAuthUser(null);
+          setLoading(false);
+        }
       }
-    });
+    );
 
     return () => {
       if (unsubscribeAuth) {
         unsubscribeAuth();
-      }
-      if (unsubscribeUser) {
-        unsubscribeUser();
       }
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   if (loading) {
-    <div className="z-50 flex h-screen w-screen items-center justify-center">
+    <div className="flex-container z-50 h-screen w-screen">
       <Spinner size="xl" />
     </div>;
   }
