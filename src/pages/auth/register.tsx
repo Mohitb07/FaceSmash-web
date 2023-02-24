ed87e400b9a792d368183969df4573ea109fa6ce
Mohitb07 <bmohit980@gmail.com>
2023-02-24 20:43:39 +0530

    refactor: now using react-hook-form

--- src/pages/auth/register.tsx
+++ src/pages/auth/register.tsx
@@ -4,7 +4,12 @@ import {
   Text,
   useBoolean,
 } from '@chakra-ui/react';
-import React, { useState } from 'react';
+import { yupResolver } from '@hookform/resolvers/yup';
+import React from 'react';
+import type { SubmitHandler } from 'react-hook-form';
+import { Controller } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
 
 import Button from '@/components/Button';
 import ErrorLabel from '@/components/ErrorLabel';
@@ -13,47 +18,51 @@ import { useRegister } from '@/hooks/useRegister';
 import AuthLayout from '@/layouts/Auth';
 import { withPublic } from '@/routes/WithPublic';
 
+const schema = yup
+  .object({
+    username: yup
+      .string()
+      .max(30, 'Username cannot exceed 30 characters')
+      .required('Username is a required field'),
+    email: yup
+      .string()
+      .email('Email must be a valid email')
+      .required('Email is a required field'),
+    password: yup
+      .string()
+      .required('Password is a required field')
+      .min(6, 'Password must be at least 6 characters'),
+    confirmPassword: yup
+      .string()
+      .required('Confirm Password is a required field')
+      .oneOf([yup.ref('password'), ''], 'Password must match'),
+  })
+  .required();
+
+type FormInput = yup.InferType<typeof schema>;
+
 const Register = () => {
-  const [registerFieldData, setRegisterFieldData] = useState({
-    username: '',
-    email: '',
-    password: '',
-    confirmPassword: '',
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+    watch,
+  } = useForm<FormInput>({
+    resolver: yupResolver(schema),
+    defaultValues: {
+      username: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+    },
   });
-  const [charactersLeft, setCharactersLeft] = useState(30);
   const [isShown, setIsShown] = useBoolean(false);
   const { loading, error, onSignUp } = useRegister();
+  const username = watch('username');
+  const charactersLeft = 30 - (username?.length || 0);
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setRegisterFieldData((prev) => ({
-      ...prev,
-      [event.target.name]: event.target.value,
-    }));
-  };
-
-  const handleUsername = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.value.length > 30) {
-      return;
-    }
-    setRegisterFieldData((prev) => ({
-      ...prev,
-      username: event.target.value,
-    }));
-    setCharactersLeft(30 - event.target.value.length);
-  };
-
-  const isPasswordMatch =
-    registerFieldData.password === registerFieldData.confirmPassword;
-  const confirmPasswordLabel = "Password doesn't match";
-
-  const onRegisterAttempt = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    onSignUp(
-      registerFieldData.username,
-      registerFieldData.email,
-      registerFieldData.password
-    );
-  };
+  const onRegisterAttempt: SubmitHandler<FormInput> = (data) =>
+    onSignUp(data.username, data.email, data.password);
 
   return (
     <AuthLayout
@@ -65,104 +74,126 @@ const Register = () => {
     >
       <form
         noValidate
-        onSubmit={onRegisterAttempt}
+        onSubmit={handleSubmit(onRegisterAttempt)}
         className="mt-5 flex flex-col space-y-2"
       >
         <div className="flex flex-col space-y-1">
           <label htmlFor="username" className="form-label-text">
             Username
           </label>
-          <Input
-            isDisabled={loading}
-            value={registerFieldData.username}
-            onChange={handleUsername}
-            isInvalid={charactersLeft === 0}
-            id="username"
+          <Controller
             name="username"
-            placeholder="Enter your username"
-            type="text"
+            control={control}
+            render={({ field }) => (
+              <Input
+                {...field}
+                maxLength={30}
+                isInvalid={Boolean(errors.username?.message)}
+                isDisabled={loading}
+                id="username"
+                name="username"
+                placeholder="Enter your username"
+                type="text"
+              />
+            )}
           />
-          {registerFieldData.username.length > 0 && (
+          {username.length > 0 && (
             <Text
               fontSize="sm"
               paddingLeft="4"
               color={charactersLeft > 0 ? 'white' : 'crimson'}
             >
-              Characters left {charactersLeft}
+              Characters left: {charactersLeft}
             </Text>
           )}
+          <ErrorLabel validationError={errors.username?.message} />
         </div>
         <div className="flex flex-col space-y-1">
           <label htmlFor="email" className="form-label-text">
             Email
           </label>
-          <Input
-            isDisabled={loading}
-            value={registerFieldData.email}
-            onChange={handleChange}
-            isInvalid={Boolean(error.email)}
-            id="email"
+          <Controller
             name="email"
-            placeholder="Email"
-            type="email"
+            control={control}
+            render={({ field }) => (
+              <Input
+                {...field}
+                isDisabled={loading}
+                isInvalid={
+                  Boolean(error.email) || Boolean(errors?.email?.message)
+                }
+                id="email"
+                name="email"
+                placeholder="Email"
+                type="email"
+              />
+            )}
           />
-          <ErrorLabel error={error.email} />
+          <ErrorLabel
+            error={error.email}
+            validationError={errors.email?.message}
+          />
         </div>
         <div className="flex flex-col space-y-1">
           <label htmlFor="password" className="form-label-text">
             Password
           </label>
-          <InputGroup size="lg">
-            <Input
-              isDisabled={loading}
-              value={registerFieldData.password}
-              isInvalid={Boolean(error.password)}
-              id="password"
-              name="password"
-              placeholder="Password"
-              type={isShown ? 'text' : 'password'}
-              onChange={handleChange}
-            />
-            {registerFieldData.password.length > 0 && (
-              <InputRightElement width="4.5rem">
-                <Button h="1.75rem" size="sm" onClick={setIsShown.toggle}>
-                  {isShown ? 'Hide' : 'Show'}
-                </Button>
-              </InputRightElement>
+          <Controller
+            name="password"
+            control={control}
+            render={({ field }) => (
+              <InputGroup size="lg">
+                <Input
+                  {...field}
+                  isDisabled={loading}
+                  isInvalid={
+                    Boolean(error.password) || Boolean(errors.password?.message)
+                  }
+                  id="password"
+                  name="password"
+                  placeholder="Password"
+                  type={isShown ? 'text' : 'password'}
+                />
+                <InputRightElement width="4.5rem">
+                  <Button h="1.75rem" size="sm" onClick={setIsShown.toggle}>
+                    {isShown ? 'Hide' : 'Show'}
+                  </Button>
+                </InputRightElement>
+              </InputGroup>
             )}
-          </InputGroup>
-          <ErrorLabel error={error.password} />
+          />
+          <ErrorLabel
+            error={error.password}
+            validationError={errors.password?.message}
+          />
         </div>
         <div className="flex flex-col space-y-1 pb-5">
-          <label htmlFor="password" className="form-label-text">
+          <label htmlFor="confirmPassword" className="form-label-text">
             Confirm Password
           </label>
-          <InputGroup size="lg">
-            <Input
-              isDisabled={loading}
-              value={registerFieldData.confirmPassword}
-              isInvalid={
-                registerFieldData.confirmPassword.length > 0 && !isPasswordMatch
-              }
-              id="confirmPassword"
-              name="confirmPassword"
-              placeholder="Re-Enter your password"
-              type={isShown ? 'text' : 'password'}
-              onChange={handleChange}
-            />
-            {registerFieldData.confirmPassword.length > 0 && (
-              <InputRightElement width="4.5rem">
-                <Button h="1.75rem" size="sm" onClick={setIsShown.toggle}>
-                  {isShown ? 'Hide' : 'Show'}
-                </Button>
-              </InputRightElement>
+          <Controller
+            name="confirmPassword"
+            control={control}
+            render={({ field }) => (
+              <InputGroup size="lg">
+                <Input
+                  {...field}
+                  isDisabled={loading}
+                  isInvalid={Boolean(errors.confirmPassword?.message)}
+                  id="confirmPassword"
+                  name="confirmPassword"
+                  placeholder="Confirm Password"
+                  type={isShown ? 'text' : 'password'}
+                />
+                <InputRightElement width="4.5rem">
+                  <Button h="1.75rem" size="sm" onClick={setIsShown.toggle}>
+                    {isShown ? 'Hide' : 'Show'}
+                  </Button>
+                </InputRightElement>
+              </InputGroup>
             )}
-          </InputGroup>
-          {registerFieldData.confirmPassword.length > 0 && !isPasswordMatch && (
-            <Text fontSize="medium" color="crimson">
-              {confirmPasswordLabel}
-            </Text>
-          )}
+          />
+          <ErrorLabel validationError={errors.confirmPassword?.message} />
         </div>
         <Button type="submit" isLoading={loading} isDisabled={loading}>
           Sign Up
