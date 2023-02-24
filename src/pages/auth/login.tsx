b823336087774f91cc192ccd5f5fe172cfb39edd
Mohitb07 <bmohit980@gmail.com>
2023-02-24 20:43:02 +0530

    refactor: proper validation error provided in schema

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -15,8 +15,11 @@ import { withPublic } from '@/routes/WithPublic';
 
 const schema = yup
   .object({
-    email: yup.string().email().required(),
-    password: yup.string().required(),
+    email: yup
+      .string()
+      .email('Email must be a valid email')
+      .required('Email is a required field'),
+    password: yup.string().required('Password is a required field'),
   })
   .required();
 
