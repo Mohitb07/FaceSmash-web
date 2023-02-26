0463fa042e264a00b3cdcec4240e073f3423a580
Mohitb07 <bmohit980@gmail.com>
2023-02-26 21:48:32 +0530

    refactor: now using react-hook-form and react-files

--- src/components/UpdateProfileModal/index.tsx
+++ src/components/UpdateProfileModal/index.tsx
@@ -13,30 +13,32 @@ import {
   ModalOverlay,
   Text,
 } from '@chakra-ui/react';
+import { yupResolver } from '@hookform/resolvers/yup';
 import { doc, updateDoc } from 'firebase/firestore';
-import React, { memo, useRef, useState } from 'react';
+import React, { memo, useState } from 'react';
+import Files from 'react-files';
+import { Controller } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
+import type { SubmitHandler } from 'react-hook-form/dist/types';
+import * as yup from 'yup';
 
 import Input from '@/components/Input';
 import { DEFAULT_PROFILE_PIC, USERS_COLLECTION } from '@/constant';
 import { useAuthUser } from '@/hooks/useAuthUser';
 import { useImageUpload } from '@/hooks/useImageUpload';
-import { convertImageObject } from '@/utils/convertImageObject';
+import type { CustomFile } from '@/interface';
 
 import { db } from '../../../firebase';
+import ErrorLabel from '../ErrorLabel';
 
 type UpdateProfileModalProps = {
   onClose: () => void;
   isOpen: boolean;
 };
 
-type UpdateProfileData = {
-  selectedImage: Blob | MediaSource | '';
-  bio: string;
-};
-
 type ProfilePicProps = {
   username: string;
-  selectedImage?: Blob | MediaSource;
+  selectedImage?: string;
   defaultImage?: string;
 };
 
@@ -52,57 +54,59 @@ const ProfilePic = ({
     ignoreFallback
     size="2xl"
     name={username}
-    src={selectedImage ? URL.createObjectURL(selectedImage) : defaultImage}
+    src={selectedImage ?? defaultImage}
   />
 );
 
 const MemoizedProfile = memo(ProfilePic);
 
+const schema = yup.object({
+  bio: yup.string().max(30, 'Bio cannot exceed 30 characters'),
+});
+
+type FormData = yup.InferType<typeof schema>;
+
+type FileInput = {
+  file: CustomFile | null;
+  error: string;
+};
+
 const UpdateProfileModal = ({ onClose, isOpen }: UpdateProfileModalProps) => {
   const { authUser } = useAuthUser();
   const [loading, setLoading] = useState(false);
-  const [updateProfileFieldData, setUpdateProfileFieldData] =
-    useState<UpdateProfileData>({
-      selectedImage: '',
+  const [fileInput, setFileInput] = useState<FileInput>({
+    file: null,
+    error: '',
+  });
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+    watch,
+  } = useForm<FormData>({
+    mode: 'all',
+    resolver: yupResolver(schema),
+    defaultValues: {
       bio: authUser?.bio || '',
-    });
+    },
+  });
+  const bio = watch('bio');
   const { uploadImage } = useImageUpload();
-  const imageInputRef = useRef<HTMLInputElement>(null);
 
-  const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) =>
-    setUpdateProfileFieldData((prev) => ({
-      ...prev,
-      bio: e.target.value,
-    }));
-
-  const handleImageProcessing = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const file = convertImageObject(e);
-    if (file) {
-      setUpdateProfileFieldData((prev) => ({
-        ...prev,
-        selectedImage: file,
-      }));
-    }
-  };
-
-  const handleProfileUpdate = async () => {
+  const handleProfileUpdate: SubmitHandler<FormData> = async (data) => {
     try {
       setLoading(true);
-      if (updateProfileFieldData.selectedImage && authUser) {
+      if (fileInput.file && authUser) {
         const urlRef = `${authUser.uid}/profilePic/`;
-        uploadImage(
-          urlRef,
-          updateProfileFieldData.selectedImage,
-          async (url: string) => {
-            await updateDoc(doc(db, USERS_COLLECTION, authUser.uid), {
-              profilePic: url,
-            });
-          }
-        );
+        uploadImage(urlRef, fileInput.file, async (url: string) => {
+          await updateDoc(doc(db, USERS_COLLECTION, authUser.uid), {
+            profilePic: url,
+          });
+        });
       }
-      if (updateProfileFieldData.bio && authUser) {
+      if (data.bio && authUser) {
         await updateDoc(doc(db, USERS_COLLECTION, authUser.uid), {
-          bio: updateProfileFieldData.bio,
+          bio: data.bio,
         });
       }
     } catch (error) {
@@ -114,9 +118,27 @@ const UpdateProfileModal = ({ onClose, isOpen }: UpdateProfileModalProps) => {
   };
 
   const isDisabled =
-    !updateProfileFieldData.bio ||
-    (updateProfileFieldData.bio === authUser?.bio &&
-      !updateProfileFieldData.selectedImage);
+    Boolean(errors.bio) ||
+    Boolean(fileInput.error) ||
+    !bio ||
+    (bio === authUser?.bio && !fileInput.file) ||
+    loading;
+
+  const handleChange = (files: CustomFile[]) => {
+    if (files.length > 0) {
+      setFileInput({
+        file: files[0],
+        error: '',
+      });
+    }
+  };
+
+  const handleError = (error: any) => {
+    setFileInput((prev) => ({
+      ...prev,
+      error: error.message,
+    }));
+  };
 
   return (
     <Modal
@@ -126,78 +148,78 @@ const UpdateProfileModal = ({ onClose, isOpen }: UpdateProfileModalProps) => {
       motionPreset="slideInBottom"
     >
       <ModalOverlay />
-      <ModalContent>
-        <ModalHeader>
-          <Text fontSize="2xl">Edit Profile</Text>
-        </ModalHeader>
-        <ModalCloseButton disabled={loading} />
-        <ModalBody>
-          <Box p={1}>
-            <FormControl isInvalid={false}>
-              <FormLabel mt={5}>Change profile pic</FormLabel>
-              <div className="flex justify-center overflow-hidden px-3">
-                <div
-                  className="inline-block cursor-pointer"
-                  onClick={() =>
-                    imageInputRef.current && imageInputRef.current.click()
-                  }
-                >
-                  {updateProfileFieldData.selectedImage ? (
-                    <MemoizedProfile
-                      username={authUser?.username || ''}
-                      selectedImage={updateProfileFieldData.selectedImage}
-                    />
-                  ) : (
-                    <MemoizedProfile
-                      username={authUser?.username || ''}
-                      defaultImage={authUser?.profilePic || DEFAULT_PROFILE_PIC}
+      <form onSubmit={handleSubmit(handleProfileUpdate)}>
+        <ModalContent>
+          <ModalHeader>
+            <Text fontSize="2xl">Edit Profile</Text>
+          </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Box p={1}>
+              <FormControl isInvalid={false}>
+                <FormLabel mt={5}>Change profile pic</FormLabel>
+                <div className="flex justify-center overflow-hidden px-3">
+                  <div className="inline-block cursor-pointer">
+                    <Files
+                      onChange={handleChange}
+                      onError={handleError}
+                      accepts={['image/*']}
+                      maxFileSize={1000000}
+                      minFileSize={0}
+                      clickable
+                    >
+                      <MemoizedProfile
+                        username={authUser?.username || ''}
+                        selectedImage={fileInput.file?.preview.url}
+                        defaultImage={
+                          authUser?.profilePic || DEFAULT_PROFILE_PIC
+                        }
+                      />
+                    </Files>
+                  </div>
+                </div>
+                <ErrorLabel validationError={fileInput.error} />
+                <FormLabel>Change your Bio</FormLabel>
+                <Controller
+                  name="bio"
+                  control={control}
+                  render={({ field }) => (
+                    <Input
+                      {...field}
+                      autoFocus
+                      name="bio"
+                      placeholder="Your bio"
                     />
                   )}
-                  <Input
-                    ref={imageInputRef}
-                    hidden
-                    isDisabled={loading}
-                    name="image"
-                    type="file"
-                    onChange={handleImageProcessing}
-                  />
-                </div>
-              </div>
-              <FormLabel>Change your Bio</FormLabel>
-              <Input
-                isDisabled={loading}
-                autoFocus
-                name="bio"
-                placeholder="Your bio"
-                value={updateProfileFieldData.bio}
-                onChange={onChangeHandler}
-              />
-            </FormControl>
-          </Box>
-        </ModalBody>
-        <ModalFooter>
-          <Button
-            disabled={loading}
-            color="white"
-            colorScheme="ghost"
-            mr={3}
-            onClick={onClose}
-          >
-            Cancel
-          </Button>
-          <Button
-            disabled={isDisabled}
-            onClick={handleProfileUpdate}
-            rounded="full"
-            color="white"
-            colorScheme="brand"
-            variant="solid"
-            isLoading={loading}
-          >
-            Update
-          </Button>
-        </ModalFooter>
-      </ModalContent>
+                />
+                <ErrorLabel validationError={errors.bio?.message} />
+              </FormControl>
+            </Box>
+          </ModalBody>
+          <ModalFooter>
+            <Button
+              isDisabled={isDisabled}
+              color="white"
+              colorScheme="ghost"
+              mr={3}
+              onClick={onClose}
+            >
+              Cancel
+            </Button>
+            <Button
+              isDisabled={isDisabled}
+              isLoading={loading}
+              rounded="full"
+              color="white"
+              colorScheme="brand"
+              variant="solid"
+              type="submit"
+            >
+              Update
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </form>
     </Modal>
   );
 };
