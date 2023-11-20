c5ce197a1ee211d09400c79cbbd8dabe13ed3d0f
Mohitb07 <bmohit980@gmail.com>
2023-11-20 21:54:18 +0530

    refactor: data list footer component

--- src/components/DataList/Footer/index.tsx
+++ src/components/DataList/Footer/index.tsx
@@ -4,18 +4,18 @@ import React from 'react';
 const Footer = ({ isLoading }: { isLoading: boolean }) => {
   if (isLoading) {
     return (
-      <div className="w-full space-y-5 rounded-md md:w-[480px]">
-        <div className="space-y-2">
+      <div className="max-w-2xl flex-1 space-y-10 rounded-2xl">
+        <div className="w-full space-y-2">
           <SkeletonCircle height="14" width="14" />
           <Skeleton height="500px"></Skeleton>
           <SkeletonText spacing="4" />
         </div>
-        <div className="space-y-2">
+        <div className="w-full space-y-2">
           <SkeletonCircle height="14" width="14" />
           <Skeleton height="500px"></Skeleton>
           <SkeletonText spacing="4" />
         </div>
-        <div className="space-y-2">
+        <div className="w-full space-y-2">
           <SkeletonCircle height="14" width="14" />
           <Skeleton height="500px"></Skeleton>
           <SkeletonText spacing="4" />
