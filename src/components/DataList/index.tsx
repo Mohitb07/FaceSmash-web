1075ecc2e6ed02a22f557a278dd7593dd92c70e3
Mohitb07 <bmohit980@gmail.com>
2023-11-20 11:23:37 +0530

    refactor: datalist component to improve scrolling and layout

--- src/components/DataList/index.tsx
+++ src/components/DataList/index.tsx
@@ -24,20 +24,38 @@ const DataList = <D extends { key: string }>({
     return <ListEmptyComponent />;
   }
   return (
-    <InfiniteScroll
-      dataLength={data.length}
-      next={getMore}
-      scrollThreshold={0.9}
-      hasMore={Boolean(lastVisible)}
-      loader
-    >
-      <div className="space-y-8">
-        {data.map((item) => (
-          <div key={item.key}>{renderItem(item)}</div>
-        ))}
-        <Footer isLoading={isLoading} />
+    <div className="relative h-[calc(100vh+4rem)] w-full overflow-hidden pb-10 md:h-screen md:pb-0">
+      <div
+        id="scrollableDiv"
+        // className="absolute inset-0 mx-auto w-full overflow-y-auto bg-red-500 pt-5 pb-10 md:w-1/2 md:pb-0"
+        className="absolute inset-0 mx-auto w-full overflow-y-auto pt-5 pb-20 md:w-full md:pb-0"
+      >
+        <InfiniteScroll
+          dataLength={data.length}
+          next={getMore}
+          hasMore={Boolean(lastVisible)}
+          loader
+          endMessage={
+            <h4 className="mt-2 text-center text-slate-500">
+              Yay! You have seen it all
+            </h4>
+          }
+          scrollThreshold={0.9}
+          scrollableTarget="scrollableDiv"
+        >
+          <div className="mx-auto flex w-full flex-col space-y-10 overflow-y-auto md:w-1/2">
+            {data.map((item) => (
+              <div className="place-items-center" key={item.key}>
+                {renderItem(item)}
+              </div>
+            ))}
+          </div>
+          <div className="flex w-full justify-center">
+            <Footer isLoading={isLoading} />
+          </div>
+        </InfiniteScroll>
       </div>
-    </InfiniteScroll>
+    </div>
   );
 };
 
