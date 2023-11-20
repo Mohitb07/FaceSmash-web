c25b192c995cfbe37a1718600234508cbe79d35d
Mohitb07 <bmohit980@gmail.com>
2023-11-20 21:47:38 +0530

    refactor: update feed limit constant

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,7 +1,7 @@
 export const DEFAULT_PROFILE_PIC =
   'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTG3eLpTAMWO-mtILepXLwg68-IChyGcXJgog&usqp=CAU';
 
-export const FEED_LIMIT = 5;
+export const FEED_LIMIT = 6;
 export const USERS_LIMIT = 5;
 
 export const USERS_COLLECTION = 'Users';
