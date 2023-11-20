84c13a91cc0ee62713f7b549c9931670fb6ea9a6
Mohitb07 <bmohit980@gmail.com>
2023-11-20 21:53:52 +0530

    refactor: datalist component and add user detail component

--- src/components/DataList/index.tsx
+++ src/components/DataList/index.tsx
@@ -1,6 +1,11 @@
-import type { DocumentSnapshot } from 'firebase/firestore';
+import type { DocumentData, DocumentSnapshot, Query } from 'firebase/firestore';
+import { ErrorBoundary } from 'react-error-boundary';
 import InfiniteScroll from 'react-infinite-scroll-component';
 
+import type { User } from '@/interface';
+
+import ErrorFallback from '../Error';
+import UserDetail from '../Profile/UserDetails';
 import Footer from './Footer';
 
 type DataListProps<D> = {
@@ -10,6 +15,11 @@ type DataListProps<D> = {
   renderItem: <T>(item: T) => JSX.Element;
   getMore: () => void;
   lastVisible: DocumentSnapshot | null;
+  isProfile?: boolean;
+  userId?: string;
+  isUserLoading?: boolean;
+  user?: User;
+  postQuery?: Query<DocumentData>;
 };
 
 const DataList = <D extends { key: string }>({
@@ -19,36 +29,68 @@ const DataList = <D extends { key: string }>({
   lastVisible,
   isLoading,
   ListEmptyComponent,
+  isProfile = false,
+  isUserLoading,
+  user,
+  postQuery,
+  userId,
 }: DataListProps<D>) => {
-  if (data.length === 0 && !isLoading) {
-    return <ListEmptyComponent />;
+  let endMessage = null;
+  if (data.length > 0 && !isLoading) {
+    endMessage = (
+      <h4 className="my-2 text-center text-slate-500">
+        Yay! You have seen it all
+      </h4>
+    );
+  } else if (data.length === 0 && !isLoading) {
+    endMessage = (
+      <h4 className="my-2 text-center text-slate-500">
+        <ListEmptyComponent />
+      </h4>
+    );
   }
+
   return (
-    <div className="relative h-[calc(100vh+4rem)] w-full overflow-hidden pb-10 md:h-screen md:pb-0">
+    <div
+      className={`relative h-[calc(100vh-3.5rem)] ${
+        isProfile ? 'md:h-[calc(100vh)]' : 'md:h-screen'
+      }`}
+    >
       <div
         id="scrollableDiv"
-        // className="absolute inset-0 mx-auto w-full overflow-y-auto bg-red-500 pt-5 pb-10 md:w-1/2 md:pb-0"
-        className="absolute inset-0 mx-auto w-full overflow-y-auto pt-5 pb-20 md:w-full md:pb-0"
+        className="absolute inset-0 overflow-y-auto scrollbar-hide"
       >
         <InfiniteScroll
           dataLength={data.length}
           next={getMore}
           hasMore={Boolean(lastVisible)}
           loader
-          endMessage={
-            <h4 className="mt-2 text-center text-slate-500">
-              Yay! You have seen it all
-            </h4>
-          }
+          endMessage={endMessage}
           scrollThreshold={0.9}
           scrollableTarget="scrollableDiv"
         >
-          <div className="mx-auto flex w-full flex-col space-y-10 overflow-y-auto md:w-1/2">
-            {data.map((item) => (
-              <div className="place-items-center" key={item.key}>
-                {renderItem(item)}
-              </div>
-            ))}
+          {isProfile && (
+            <ErrorBoundary FallbackComponent={ErrorFallback}>
+              <UserDetail
+                isLoading={isUserLoading || false}
+                user={user!}
+                userId={userId!}
+                userQuery={postQuery!}
+              />
+            </ErrorBoundary>
+          )}
+          <div
+            className={`flex h-full w-full flex-col  space-y-10 ${
+              isProfile ? 'pt-2' : 'pt-16'
+            } md:pt-[1rem]`}
+          >
+            <div className="mx-auto w-full max-w-2xl space-y-5 p-2 px-4">
+              {data.map((item) => (
+                <div className="place-items-center" key={item.key}>
+                  {renderItem(item)}
+                </div>
+              ))}
+            </div>
           </div>
           <div className="flex w-full justify-center">
             <Footer isLoading={isLoading} />
