cd2ea53d50094bffeb0a20cd06d3e26b9b64757e
Mohitb07 <bmohit980@gmail.com>
2023-11-21 14:10:48 +0530

    fix: update brand component styles

--- src/components/Brand/index.tsx
+++ src/components/Brand/index.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 const Brand = ({ styles }: { styles?: string }) => {
   return (
-    <h1 className={`text-[2rem] font-bold text-white ${styles}`}>
+    <h1
+      className={`p-3 text-2xl font-bold text-white md:text-[2rem] ${styles}`}
+    >
       <span className="text-primary-100">Face</span>Smash
     </h1>
   );
