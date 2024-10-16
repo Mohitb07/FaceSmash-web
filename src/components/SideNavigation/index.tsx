553d677e2742f08245e4416f72218fd44435d48e
mohitb07 <bmohit980@gmail.com>
2024-10-16 21:39:58 +0530

    refactor: icons changed and layout fix

--- src/components/SideNavigation/index.tsx
+++ src/components/SideNavigation/index.tsx
@@ -1,8 +1,14 @@
 import { Avatar } from '@chakra-ui/react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import React, { memo } from 'react';
+import { memo } from 'react';
+import { GoHome } from 'react-icons/go';
+// @ts-ignore
+import { GoHomeFill } from 'react-icons/go';
+// @ts-ignore
+import { IoSearch } from 'react-icons/io5';
 import { RxHamburgerMenu } from 'react-icons/rx';
+import { TbSquareRoundedPlus } from 'react-icons/tb';
 
 import Brand from '@/components/Brand';
 import NavItem from '@/components/NavItem';
@@ -23,97 +29,76 @@ const Sidebar = ({
 }: SidebarProps) => {
   const router = useRouter();
   return (
-    <div className="flex h-full flex-col justify-between p-6 lg:w-full lg:items-center">
-      <nav>
-        <div className="hidden lg:block">
-          <Brand styles="mb-[3rem] ml-[1rem]" />
-        </div>
-        <div className="block lg:hidden">
-          <h1 className="mb-[3rem] text-center text-4xl font-bold">
-            <span className="text-primary-100">F</span>
-          </h1>
-        </div>
-
-        <ul className="space-y-10 text-xl">
-          <Link href="/">
-            <NavItem
-              icon={
-                router.pathname === '/' ? (
-                  // <TiHome className="hover-animation text-4xl text-blue-500" />
-                  <img
-                    width="40"
-                    height="40"
-                    src="https://img.icons8.com/3d-fluency/94/paper-plane.png"
-                    alt="paper-plane"
+    <div className="mt-5 w-full">
+      <div className="flex h-full w-full flex-col items-center">
+        <div className="h-5/6 p-2">
+          <div>
+            <div className="mb-3 hidden lg:block">
+              <Brand />
+            </div>
+            <div className="block lg:hidden">
+              <h1 className="mb-[3rem] text-center text-4xl font-bold">
+                <span className="text-primary-100">F</span>
+              </h1>
+            </div>
+          </div>
+          <nav className="flex h-full flex-col justify-between">
+            <ul className="space-y-8">
+              <Link href="/">
+                <NavItem
+                  icon={
+                    router.pathname === '/' ? (
+                      <GoHomeFill className="text-4xl" />
+                    ) : (
+                      <GoHome className="text-4xl" />
+                    )
+                  }
+                  label="Home"
+                />
+              </Link>
+              <NavItem
+                onClick={() => setIsSearchDrawerOpen(true)}
+                icon={<IoSearch className="text-4xl" />}
+                label="Search"
+              />
+              <NavItem
+                onClick={() => setIsModalOpen(true)}
+                icon={<TbSquareRoundedPlus className="text-4xl" />}
+                label="Create"
+              />
+              <Link
+                href={{
+                  pathname: '/[username]',
+                  query: { username: user?.qusername, userId: user?.uid },
+                }}
+              >
+                <NavItem label="Profile">
+                  <Avatar
+                    role="navigation"
+                    ring={
+                      router.pathname === '/[username]' &&
+                      router.query.userId === user?.uid
+                        ? 2
+                        : 0
+                    }
+                    ringColor="white"
+                    className="hover-animation ml-1"
+                    size="sm"
+                    name={user?.username}
+                    src={user?.profilePic}
                   />
-                ) : (
-                  // <VscHome className="hover-animation text-4xl" />
-                  <img
-                    width="40"
-                    height="40"
-                    src="https://img.icons8.com/3d-fluency/94/paper-plane.png"
-                    alt="paper-plane"
-                  />
-                )
-              }
-              label="Home"
-            />
-          </Link>
-          <NavItem
-            onClick={() => setIsSearchDrawerOpen(true)}
-            icon={
-              <img
-                width="40"
-                height="40"
-                src="https://img.icons8.com/pastel-glyph/64/7950F2/search--v2.png"
-                alt="search--v2"
-              />
-            }
-            label="Search"
-          />
-          <NavItem
-            onClick={() => setIsModalOpen(true)}
-            icon={
-              <img
-                width="40"
-                height="40"
-                src="https://img.icons8.com/glyph-neue/64/7950F2/plus-2-math.png"
-                alt="plus-2-math"
-              />
-            }
-            label="Create"
-          />
-          <Link
-            href={{
-              pathname: '/[username]',
-              query: { username: user?.qusername, userId: user?.uid },
-            }}
-          >
-            <NavItem label="Profile">
-              <Avatar
-                role="navigation"
-                ring={
-                  router.pathname === '/[username]' &&
-                  router.query.userId === user?.uid
-                    ? 2
-                    : 0
-                }
-                ringColor="white"
-                className="hover-animation"
-                size="sm"
-                name={user?.username}
-                src={user?.profilePic}
-              />
-            </NavItem>
-          </Link>
-        </ul>
-      </nav>
-      <div>
-        <div className="hidden lg:block">
-          <Settings Icon={RxHamburgerMenu} label="More" />
-        </div>
-        <div className="lg:hidden">
-          <Settings Icon={RxHamburgerMenu} />
+                </NavItem>
+              </Link>
+            </ul>
+            <ul className="text-xl">
+              <div className="hidden lg:block">
+                <Settings Icon={RxHamburgerMenu} />
+              </div>
+              {/* <div className="lg:hidden">
+                <Settings Icon={RxHamburgerMenu} />
+              </div> */}
+            </ul>
+          </nav>
         </div>
       </div>
     </div>
