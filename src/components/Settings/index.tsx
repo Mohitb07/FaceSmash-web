3ea88741312ec14ad6ec7bd1109733eaba7b5dbe
mohitb07 <bmohit980@gmail.com>
2024-10-16 21:41:28 +0530

    refactor: icon changed and code refactored

--- src/components/Settings/index.tsx
+++ src/components/Settings/index.tsx
@@ -1,25 +1,25 @@
 import { Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react';
 import type { IconType } from 'react-icons';
+import { CiTrash } from 'react-icons/ci';
 import { FiSettings } from 'react-icons/fi';
 
 import { useAuthUser } from '@/hooks/useAuthUser';
+
+import NavItem from '../NavItem';
 
 type SettingsProps = {
   Icon?: IconType;
   label?: string;
 };
 
-const Settings = ({ Icon = FiSettings, label = '' }: SettingsProps) => {
+const Settings = ({ Icon = FiSettings }: SettingsProps) => {
   const { logout } = useAuthUser();
   return (
     <Menu>
       <MenuButton>
-        <ul>
-          <li className="nav-item">
-            <Icon className="ml-3 text-xl xl:text-3xl" />
-            {label.length > 0 && <p>{label}</p>}
-          </li>
-        </ul>
+        <NavItem label="More">
+          <Icon className="text-xl xl:text-3xl" />
+        </NavItem>
       </MenuButton>
       <MenuList backgroundColor="#242526" border="none" padding="2">
         <MenuItem
@@ -29,16 +29,9 @@ const Settings = ({ Icon = FiSettings, label = '' }: SettingsProps) => {
             backgroundColor: '#40404F',
             borderRadius: '3px',
           }}
-          icon={
-            <img
-              width="24"
-              height="24"
-              src="https://img.icons8.com/external-tal-revivo-bold-tal-revivo/24/ff0000/external-online-account-logout-with-arrow-direction-mark-login-bold-tal-revivo.png"
-              alt="external-online-account-logout-with-arrow-direction-mark-login-bold-tal-revivo"
-            />
-          }
+          icon={<CiTrash className="text-lg text-red-400" />}
         >
-          <button aria-label="logout user" className="font-semibold">
+          <button aria-label="logout user" className="text-lg">
             Log Out
           </button>
         </MenuItem>
