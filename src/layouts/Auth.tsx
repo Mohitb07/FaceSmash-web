44075a41469a9837a3f25275857f0965d7d3cd48
mohitb07 <bmohit980@gmail.com>
2025-02-26 15:23:42 +0530

    refactor: update Auth layout for improved styling and structure

--- src/layouts/Auth.tsx
+++ src/layouts/Auth.tsx
@@ -26,51 +26,54 @@ const Layout: React.FC<LayoutProps> = ({
     <Main
       meta={
         <Meta
-          title={`FaceSmash - ${meta}`}
+          title={`faceSmash - ${meta}`}
           description="It's a social media platform for users to interact with their friends."
         />
       }
     >
-      <div className="flex-container min-h-screen md:space-x-16">
-        <div className="lg:flex-container hidden">
-          <Image
-            src="https://i.postimg.cc/tg77N3Rm/m2.png"
-            alt="authentication page app showcase"
-            objectFit="contain"
-            height={700}
-            width={400}
-          />
-        </div>
-        <div>
-          <div
-            className={`bg-color h-screen w-screen p-5 md:h-auto md:w-[500px] md:p-16 ${containerStyle}`}
-          >
-            <header>
-              <h1 className="text-center text-5xl font-bold text-white">
-                <span className="text-primary-100">Face</span>Smash
-              </h1>
-            </header>
-            <div className="flex-1">{children}</div>
-            <div className="mt-10 text-center md:hidden">
+      <div className="layout-wrapper min-h-screen bg-black/60">
+        <div className="flex items-center gap-16">
+          <div className="hidden lg:block">
+            <Image
+              className="layout-image"
+              src="https://i.postimg.cc/tg77N3Rm/m2.png"
+              alt="authentication page app showcase"
+              objectFit="contain"
+              height={700}
+              width={400}
+            />
+          </div>
+          <div>
+            <div
+              className={`h-screen w-screen p-5 md:h-auto md:w-[500px] md:p-16 ${containerStyle} rounded-md bg-gray-900`}
+            >
+              <header>
+                <h1 className="text-center text-5xl font-bold text-white">
+                  <span className="text-primary-100">Face</span>Smash
+                </h1>
+              </header>
+              <div className="flex-1">{children}</div>
+              <div className="mt-10 text-center md:hidden">
+                <Link href={footerLink}>
+                  <h1 className="form-label-text text-sm">
+                    {footerText}{' '}
+                    <span className="font-bold text-primary-100">
+                      {footerLabel}
+                    </span>
+                  </h1>
+                </Link>
+              </div>
+            </div>
+            <div className="md:flex-container mt-5 hidden h-20 w-[500px]">
               <Link href={footerLink}>
-                <h1 className="form-label-text text-sm">
+                <h1 className="form-label-text text-lg">
                   {footerText}{' '}
-                  <span className="font-bold text-primary-100">
+                  <span className="cursor-pointer font-bold text-primary-100">
                     {footerLabel}
                   </span>
                 </h1>
               </Link>
             </div>
-          </div>
-          <div className="md:flex-container bg-color mt-5 hidden h-20 w-[500px]">
-            <Link href={footerLink}>
-              <h1 className="form-label-text text-lg">
-                {footerText}{' '}
-                <span className="cursor-pointer font-bold text-primary-100">
-                  {footerLabel}
-                </span>
-              </h1>
-            </Link>
           </div>
         </div>
       </div>
